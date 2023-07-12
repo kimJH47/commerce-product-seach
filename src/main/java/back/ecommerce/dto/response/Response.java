b5676dc3d0d9f98af9c98e4a29c08931efeb2e90
kimJaeHyeon <kmr2644@gmail.com>
2023-07-13 08:29:15 +0900

    fix: json 파싱을 위한 getter 어노테이션 추가

--- src/main/java/back/ecommerce/dto/response/Response.java
+++ src/main/java/back/ecommerce/dto/response/Response.java
@@ -3,12 +3,12 @@ package back.ecommerce.dto.response;
 import org.springframework.http.ResponseEntity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class Response {
 	private String message;
-
-
 
 	public static <T> ResponseEntity<Response> createSuccessResponse(String message, T entity) {
 		return ResponseEntity.ok()
