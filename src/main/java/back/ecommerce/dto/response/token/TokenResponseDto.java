087d5d517ac51291c423afb913b2d7b366f679d6
kimJaeHyeon <kmr2644@gmail.com>
2023-07-13 08:29:31 +0900

    refactor: final 필드로 변경

--- src/main/java/back/ecommerce/dto/response/token/TokenResponseDto.java
+++ src/main/java/back/ecommerce/dto/response/token/TokenResponseDto.java
@@ -1,12 +1,14 @@
 package back.ecommerce.dto.response.token;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class TokenResponseDto {
-	private String accessToken;
-	private int expireTime;
-	private String type;
+	private final String accessToken;
+	private final int expireTime;
+	private final String type;
 
 	public TokenResponseDto create(String token, int expireTime, String type) {
 		return new TokenResponseDto(token, expireTime, type);
