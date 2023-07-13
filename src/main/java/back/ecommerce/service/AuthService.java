71a5613a23dc456c6ed3606d2f92bbe78358b441
kimJaeHyeon <kmr2644@gmail.com>
2023-07-13 11:25:52 +0900

    feat: 토큰생성 구현

--- src/main/java/back/ecommerce/service/AuthService.java
+++ src/main/java/back/ecommerce/service/AuthService.java
@@ -1,14 +1,32 @@
 package back.ecommerce.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
+import back.ecommerce.auth.Token;
+import back.ecommerce.auth.TokenProvider;
+import back.ecommerce.domain.User;
 import back.ecommerce.dto.response.token.TokenResponseDto;
+import back.ecommerce.exception.PasswordNotMatchedException;
+import back.ecommerce.exception.UserNotFoundException;
+import back.ecommerce.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	private final TokenProvider tokenProvider;
+
 	public TokenResponseDto createToken(String email, String password) {
-		return null;
+		User user = userRepository.findByEmail(email)
+			.orElseThrow(() -> new UserNotFoundException("해당하는 유저가 존재하지 않습니다."));
+
+		if (!passwordEncoder.matches(user.getPassword(), password)) {
+			throw new PasswordNotMatchedException("비밀번호가 일치하지 않습니다.");
+		}
+		Token token = tokenProvider.create(email);
+		return TokenResponseDto.create(token.getValue(), token.getExpireTime(), token.getType());
 	}
 }
