1f3eb0ae6e38ee2486dedf8e5c1320d5acd95c9f
kimJaeHyeon <kmr2644@gmail.com>
2023-07-13 22:14:12 +0900

    feat: UserNotFoundException, PasswordNotMatchedException 핸들링 메서드 구현

--- src/main/java/back/ecommerce/controller/GlobalExceptionHandler.java
+++ src/main/java/back/ecommerce/controller/GlobalExceptionHandler.java
@@ -6,13 +6,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import back.ecommerce.dto.response.Response;
+import back.ecommerce.exception.PasswordNotMatchedException;
+import back.ecommerce.exception.UserNotFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 	private static final String BAD_REQUEST = "잘못된 요청입니다.";
+
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<Response> handle(MethodArgumentNotValidException e) {
 		return Response.createBadRequest(BAD_REQUEST, e);
+	}
+
+	@ExceptionHandler(value = {UserNotFoundException.class, PasswordNotMatchedException.class})
+	public ResponseEntity<Response> handle(RuntimeException e) {
+		return Response.createBadRequest(BAD_REQUEST, "login", e.getMessage());
 	}
 }
