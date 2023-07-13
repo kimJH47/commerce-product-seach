b8ef48ba5213b70b583733b711f603c67bc2ada2
kimJaeHyeon <kmr2644@gmail.com>
2023-07-13 09:45:35 +0900

    feat: status 404 실패 response 구현

---
+++ src/main/java/back/ecommerce/dto/response/FailedResponse.java
@@ -0,0 +1,24 @@
+package back.ecommerce.dto.response;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import lombok.Getter;
+
+@Getter
+public class FailedResponse extends Response{
+	private final Map<String, String> reasons = new HashMap<>();
+
+	public FailedResponse(String message, MethodArgumentNotValidException e) {
+		super(message);
+		List<FieldError> fieldErrors = e.getFieldErrors();
+		for (FieldError fieldError : fieldErrors) {
+			reasons.put(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+	}
+
+}
--- src/main/java/back/ecommerce/dto/response/Response.java
+++ src/main/java/back/ecommerce/dto/response/Response.java
@@ -1,6 +1,7 @@
 package back.ecommerce.dto.response;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,5 +14,10 @@ public class Response {
 	public static <T> ResponseEntity<Response> createSuccessResponse(String message, T entity) {
 		return ResponseEntity.ok()
 			.body(new SuccessResponse<>(message, entity));
+	}
+
+	public static ResponseEntity<Response> createBadRequest(String message, MethodArgumentNotValidException e) {
+		return ResponseEntity.badRequest()
+			.body(new FailedResponse(message, e));
 	}
 }
