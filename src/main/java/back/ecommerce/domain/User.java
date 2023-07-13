5be2b53182c61934399576aed895cf18768d704c
kimJaeHyeon <kmr2644@gmail.com>
2023-07-13 22:51:52 +0900

    feat: equals, hashCode 오버라이딩

--- src/main/java/back/ecommerce/domain/User.java
+++ src/main/java/back/ecommerce/domain/User.java
@@ -1,5 +1,7 @@
 package back.ecommerce.domain;
 
+import java.util.Objects;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -23,5 +25,21 @@ public class User extends BaseTimeEntity {
 
 	public String getPassword() {
 		return password;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		User user = (User)o;
+		return Objects.equals(id, user.id) && Objects.equals(email, user.email)
+			&& Objects.equals(password, user.password);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, email, password);
 	}
 }
