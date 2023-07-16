7194efc01e11bbd10e8ee913d96055a888bd2f62
kimJaeHyeon <kmr2644@gmail.com>
2023-07-16 20:32:21 +0900

    refactor: 카트에 상품추가 후 조회기능 분리

--- src/main/java/back/ecommerce/controller/CartController.java
+++ src/main/java/back/ecommerce/controller/CartController.java
@@ -22,7 +22,8 @@ public class CartController {
 
 	@PostMapping("/add-product")
 	public ResponseEntity<Response> addCart(@RequestBody @Valid AddCartRequest request) {
+		cartService.addProduct(request.getEmail(), request.getProductId(), request.getQuantity());
 		return Response.createSuccessResponse("상품이 카트에 성공적으로 추가 되었습니다.",
-			cartService.addProduct(request.getEmail(), request.getProductId(), request.getQuantity()));
+			cartService.findCartByUserEmail(request.getEmail()));
 	}
 }
