c88c15a244280987e68c3493f7b1d59308b600e0
kimJaeHyeon <kmr2644@gmail.com>
2023-07-20 06:16:05 +0900

    test: exists email db 테스트 작성

--- src/test/java/back/ecommerce/repository/UserRepositoryTest.java
+++ src/test/java/back/ecommerce/repository/UserRepositoryTest.java
@@ -14,25 +14,41 @@ class UserRepositoryTest {
 	@Autowired
 	UserRepository userRepository;
 
-
 	@Test
 	@DisplayName("사용자의 이메일로 조회가 성공적으로 되어야한다.")
 	void findByEmail() {
 		//given
-		User expected = new User(1L,"email1@email.com","dsdaml!edmslm");
-		User user2 = new User(2L,"emall2@email.com","sddmmdlskamlk12das");
-		User user3 = new User(3L,"email3@email.com","sddmmdlskamlk13");
-		userRepository.save(expected);
-		userRepository.save(user2);
-		userRepository.save(user3);
+		User expected = userRepository.save(new User(10L, "email1@email.com", "dsdaml!edmslm"));
+		userRepository.save(new User(null, "emall2@email.com", "sddmmdlskamlk12das"));
+		userRepository.save(new User(null, "email3@email.com", "sddmmdlskamlk13"));
 
-	    //when
+		//when
 		User actual = userRepository.findByEmail("email1@email.com")
 			.orElse(new User(-1000L, "", ""));
 
 		//then
 		assertThat(actual).isEqualTo(expected);
+	}
 
+	@Test
+	@DisplayName("사용자의 이메일이 존재하는지 하고 존재하면 true 를 반환해야한다.")
+	void existsByEmail_true() {
+		//given
+		String email = "email1@email.com";
+		userRepository.save(new User(1L, email, "dsdaml!edmss,xmlm"));
+
+		//expect
+		assertThat(userRepository.existsByEmail(email)).isTrue();
+	}
+
+	@Test
+	@DisplayName("사용자의 이메일이 존재하는지 하고 존재하지 않으면 false 를 반환해야한다.")
+	void existsByEmail_false() {
+		//given
+		userRepository.save(new User(1L, "email1@email.com", "dsdamlmss,xmlm"));
+
+		//expect
+		assertThat(userRepository.existsByEmail("other@email.com")).isFalse();
 
 	}
 }
