66439203416581c7acf148a08c9d0ef30abe4422
kimJaeHyeon <kmr2644@gmail.com>
2023-07-23 12:31:36 +0900

    feat: userEmail 어노테이션 argumentResolver 구현

---
+++ src/main/java/back/ecommerce/auth/resolver/UserEmailArgumentResolver.java
@@ -0,0 +1,34 @@
+package back.ecommerce.auth.resolver;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import back.ecommerce.auth.annotaion.UserEmail;
+import back.ecommerce.exception.TokenHasInvalidException;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UserEmailArgumentResolver implements HandlerMethodArgumentResolver {
+
+	private static final String TOKEN_ATTRIBUTE = "email";
+
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) {
+		return parameter.getParameterAnnotation(UserEmail.class) != null
+			&& parameter.getParameterType().equals(String.class);
+	}
+
+	@Override
+	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+		NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+		Object email = webRequest.getAttribute(TOKEN_ATTRIBUTE, RequestAttributes.SCOPE_REQUEST);
+		if (email == null) {
+			throw new TokenHasInvalidException("인증이 성공적으로 되지 않았습니다.");
+		}
+		return email;
+	}
+}
--- src/main/java/back/ecommerce/config/WebMvcConfig.java
+++ src/main/java/back/ecommerce/config/WebMvcConfig.java
@@ -1,11 +1,15 @@
 package back.ecommerce.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import back.ecommerce.auth.interceptor.JwtAuthenticationInterceptor;
+import back.ecommerce.auth.resolver.UserEmailArgumentResolver;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -19,5 +23,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(interceptor)
 			.addPathPatterns("/api/cart/**");
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(new UserEmailArgumentResolver());
 	}
 }
