854afd983c922783bd289a2e35e2303795404296
kimJaeHyeon <kmr2644@gmail.com>
2023-07-23 23:10:27 +0900

    feat: findByUserEmail 시 패치조인으로 조회

--- src/main/java/back/ecommerce/repository/CartRepository.java
+++ src/main/java/back/ecommerce/repository/CartRepository.java
@@ -3,9 +3,13 @@ package back.ecommerce.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import back.ecommerce.domain.Cart;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
-	List<Cart> findByUserEmail(String userEmail);
+
+	@Query("select c from Cart c join fetch c.product where c.userEmail =:userEmail")
+	List<Cart> findByUserEmail(@Param("userEmail") String userEmail);
 }
