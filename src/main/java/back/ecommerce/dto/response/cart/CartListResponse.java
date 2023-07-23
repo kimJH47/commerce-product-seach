d94f96b74d52b2f26f5e2fa3a729a973af9c314c
kimJaeHyeon <kmr2644@gmail.com>
2023-07-23 13:14:36 +0900

    feat: 장바구니 상품을 담고있는 CartProducts 구현 및 CartListResponse 스펙 변경

--- src/main/java/back/ecommerce/dto/response/cart/CartListResponse.java
+++ src/main/java/back/ecommerce/dto/response/cart/CartListResponse.java
@@ -1,8 +1,5 @@
 package back.ecommerce.dto.response.cart;
 
-import java.util.List;
-
-import back.ecommerce.dto.CartProductDto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -10,7 +7,5 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CartListResponse {
 	private final String email;
-	private final int count;
-	private final Long totalPrice;
-	private final List<CartProductDto> CartProducts;
+	private final CartProducts CartProducts;
 }
