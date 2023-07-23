67f6a439f478348d11a089142a7cdbd0c11afe6a
kimJaeHyeon <kmr2644@gmail.com>
2023-07-23 12:37:02 +0900

    test: 사용자 장바구니 조회 api 테스트 작성

--- src/test/java/back/ecommerce/controller/CartControllerTest.java
+++ src/test/java/back/ecommerce/controller/CartControllerTest.java
@@ -10,23 +10,30 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import back.ecommerce.auth.annotaion.UserEmail;
 import back.ecommerce.domain.product.Category;
 import back.ecommerce.dto.CartProductDto;
 import back.ecommerce.dto.request.AddCartRequest;
@@ -41,11 +48,18 @@ import back.ecommerce.service.CartService;
 @Import(MockAuthProviderConfig.class)
 class CartControllerTest {
 
-	@Autowired
 	MockMvc mockMvc;
 	@MockBean
 	CartService cartService;
 	ObjectMapper mapper = new ObjectMapper();
+
+	@BeforeEach
+	void setUp() {
+		mockMvc = MockMvcBuilders.standaloneSetup(new CartController(cartService))
+			.setCustomArgumentResolvers(new MockUserEmailArgumentResolver())
+			.setControllerAdvice(new GlobalExceptionHandler())
+			.build();
+	}
 
 	@Test
 	@DisplayName("/api/cart/add-product POST 요청을 보내면 응답코드 200과 함께 등록된 카트 리스트가 응답되어야한다.")
@@ -109,7 +123,7 @@ class CartControllerTest {
 	}
 
 	@Test
-	@DisplayName("/api/cart/add-product POST 로 존재하지 않는 사용자 이메일을 요청으로 보내면 응답코드 400")
+	@DisplayName("/api/cart/add-product POST 로 존재하지 않는 사용자 이메일을 요청으로 보내면 응답코드 400과 함께 실패이유가 응답되어야한다.")
 	void add_product_userNotFoundException() throws Exception {
 		//given
 		doThrow(new UserNotFoundException("해당하는 유저가 존재하지 않습니다."))
@@ -124,7 +138,7 @@ class CartControllerTest {
 	}
 
 	@Test
-	@DisplayName("")
+	@DisplayName("/api/cart/add-product POST 로 존재하지 않는 상품 아이디를 요청으로 보내면 응답코드 400과 함께 실패이유가 응답되어야한다.")
 	void add_product_productNotFoundException() throws Exception {
 		//given
 		doThrow(new ProductNotFoundException("해당하는 상품이 존재하지 않습니다."))
@@ -139,9 +153,75 @@ class CartControllerTest {
 
 	}
 
+	@Test
+	@DisplayName("/api/cart?email={email} GET 로 요청을 보내면 사용자의 장바구니를 응답코드 200과 함께 응답한다.")
+	void find() throws Exception {
+		//given
+		ArrayList<CartProductDto> cartProductDtos = new ArrayList<>();
+		cartProductDtos.add(createDto(10, "shirts", "coverNat", Category.TOP, 2, 10000L));
+		cartProductDtos.add(createDto(23, "cap", "carHartt", Category.HEAD_WEAR, 1, 8000L));
+		cartProductDtos.add(createDto(35, "ring", "carHartt", Category.ACCESSORY, 1, 150000L));
+		given(cartService.findCartByUserEmail(anyString()))
+			.willReturn(new CartListResponse("user@email.com", cartProductDtos.size(), 1780000L, cartProductDtos));
+		//expect
+		mockMvc.perform(get("/api/cart")
+				.param("email", "user@email.com"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.message").value("장바구니가 성곡적으로 조회 되었습니다."))
+			.andExpect(jsonPath("$.entity.email").value("user@email.com"))
+			.andExpect(jsonPath("$.entity.count").value(3))
+			.andExpect(jsonPath("$.entity.totalPrice").value(1780000))
+
+			.andExpect(jsonPath("$.entity.cartProducts[0].id").value(10))
+			.andExpect(jsonPath("$.entity.cartProducts[0].name").value("shirts"))
+			.andExpect(jsonPath("$.entity.cartProducts[0].brandName").value("coverNat"))
+			.andExpect(jsonPath("$.entity.cartProducts[0].category").value(Category.TOP.toString()))
+			.andExpect(jsonPath("$.entity.cartProducts[0].quantity").value(2))
+			.andExpect(jsonPath("$.entity.cartProducts[0].price").value(10000L))
+
+			.andExpect(jsonPath("$.entity.cartProducts[1].id").value(23))
+			.andExpect(jsonPath("$.entity.cartProducts[1].name").value("cap"))
+			.andExpect(jsonPath("$.entity.cartProducts[1].brandName").value("carHartt"))
+			.andExpect(jsonPath("$.entity.cartProducts[1].category").value(Category.HEAD_WEAR.toString()))
+			.andExpect(jsonPath("$.entity.cartProducts[1].quantity").value(1))
+			.andExpect(jsonPath("$.entity.cartProducts[1].price").value(8000L))
+
+			.andExpect(jsonPath("$.entity.cartProducts[2].id").value(35))
+			.andExpect(jsonPath("$.entity.cartProducts[2].name").value("ring"))
+			.andExpect(jsonPath("$.entity.cartProducts[2].brandName").value("carHartt"))
+			.andExpect(jsonPath("$.entity.cartProducts[2].category").value(Category.ACCESSORY.toString()))
+			.andExpect(jsonPath("$.entity.cartProducts[2].quantity").value(1))
+			.andExpect(jsonPath("$.entity.cartProducts[2].price").value(150000L));
+	}
+
+	@Test
+	@DisplayName("토큰 페이로드 이메일정보와 요청 request 에 들어오는 이메일이 일치하지 않으면 응답코드 400과 함께 실피애유가 응답 되어야한다.")
+	void find_exception() throws Exception {
+		//expect
+		mockMvc.perform(get("/api/cart")
+				.param("email", "otherUser@email.com"))
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
+			.andExpect(jsonPath("$.reasons.accessToken").value("토큰정보와 요청정보가 일치하지 않습니다."));
+	}
+
 	private CartProductDto createDto(long id, String name, String brandName, Category category, int quantity,
 		long price) {
 		return new CartProductDto(id, name, brandName, price, category, quantity);
 	}
 
+	static class MockUserEmailArgumentResolver implements HandlerMethodArgumentResolver {
+
+		@Override
+		public boolean supportsParameter(MethodParameter parameter) {
+			return parameter.getParameterAnnotation(UserEmail.class) != null
+				&& parameter.getParameterType().equals(String.class);
+		}
+
+		@Override
+		public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+			NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+			return "user@email.com";
+		}
+	}
 }
