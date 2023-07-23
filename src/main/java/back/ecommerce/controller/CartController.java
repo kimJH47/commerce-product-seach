44f96ff448d0c91506e2041e0d6bb501ab875202
kimJaeHyeon <kmr2644@gmail.com>
2023-07-23 12:36:35 +0900

    feat: 사용자 장바구니 조회 api 구현, 이 때 토큰의 이메일 정보와 request 의 이메일정보가 일치하지 않으면 응답이 실패한다.

--- src/main/java/back/ecommerce/controller/CartController.java
+++ src/main/java/back/ecommerce/controller/CartController.java
@@ -1,15 +1,20 @@
 package back.ecommerce.controller;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import back.ecommerce.auth.annotaion.UserEmail;
 import back.ecommerce.dto.request.AddCartRequest;
 import back.ecommerce.dto.response.Response;
+import back.ecommerce.exception.TokenHasInvalidException;
 import back.ecommerce.service.CartService;
 import lombok.RequiredArgsConstructor;
 
@@ -25,5 +30,16 @@ public class CartController {
 		cartService.addProduct(request.getEmail(), request.getProductId(), request.getQuantity());
 		return Response.createSuccessResponse("상품이 카트에 성공적으로 추가 되었습니다.",
 			cartService.findCartByUserEmail(request.getEmail()));
+	}
+
+	@GetMapping
+	public ResponseEntity<Response> findByEmail(
+		@UserEmail String tokenEmail,
+		@RequestParam("email") @Valid @Email String email) {
+		if (!tokenEmail.equals(email)) {
+			throw new TokenHasInvalidException("토큰정보와 요청정보가 일치하지 않습니다.");
+		}
+		return Response.createSuccessResponse("장바구니가 성곡적으로 조회 되었습니다.",
+			cartService.findCartByUserEmail(email));
 	}
 }
