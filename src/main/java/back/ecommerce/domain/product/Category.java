7daa53c73f38737f732a4737fa6a10d56891491b
kimJaeHyeon <kmr2644@gmail.com>
2023-07-29 13:59:43 +0900

    feat: Category 생성 메서드 구현

--- src/main/java/back/ecommerce/domain/product/Category.java
+++ src/main/java/back/ecommerce/domain/product/Category.java
@@ -1,5 +1,16 @@
 package back.ecommerce.domain.product;
 
+import java.util.Arrays;
+
 public enum Category {
-	TOP, OUTER, PANTS, ONEPIECE, SKIRT, SNEAKERS, SHOES, HEAD_WEAR, ACCESSORY
+	TOP, OUTER, PANTS, ONEPIECE, SKIRT, SNEAKERS, SHOES, HEAD_WEAR, ACCESSORY;
+
+	public static Category fromString(String value) {
+		return Arrays.stream(Category.values())
+			.limit(1)
+			.filter(category -> category.toString().equalsIgnoreCase(value))
+			.findAny()
+			.orElseThrow(() -> new IllegalArgumentException("일치하는 카테고리가 없습니다."));
+	}
+
 }
