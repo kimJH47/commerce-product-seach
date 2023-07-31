608f789c5eef898297c6007f3b0770aa7fca54b1
kimJaeHyeon <kmr2644@gmail.com>
2023-07-31 22:01:54 +0900

    feat: 상품 카테고리별 조회기능 구현

--- src/main/java/back/ecommerce/service/ProductService.java
+++ src/main/java/back/ecommerce/service/ProductService.java
@@ -1,13 +1,27 @@
 package back.ecommerce.service;
 
+import static back.ecommerce.constant.PageConstant.*;
+
+import java.util.List;
+
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import back.ecommerce.domain.product.Category;
+import back.ecommerce.dto.ProductDto;
 import back.ecommerce.dto.response.ProductListResponse;
+import back.ecommerce.repository.ProductQueryDslRepository;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class ProductService {
+
+	private final ProductQueryDslRepository productQueryDslRepository;
+
 	public ProductListResponse findWithCategoryAndPagination(Category category) {
-		return null;
+		List<ProductDto> products = productQueryDslRepository.findByCategoryWithPaginationOrderByBrandNew(category,
+			PageRequest.of(DEFAULT__PAGE, DEFAULT_PAGE_SIZE));
+		return new ProductListResponse(products.size(), products);
 	}
 }
