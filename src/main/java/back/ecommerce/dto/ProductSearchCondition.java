01b36abc37dafb0b58d415b8fdfef76ad13675e2
kimJaeHyeon <kmr2644@gmail.com>
2023-08-01 17:18:44 +0900

    feat: 상품검색 조건 condition 객체 구현

---
+++ src/main/java/back/ecommerce/dto/ProductSearchCondition.java
@@ -0,0 +1,39 @@
+package back.ecommerce.dto;
+
+import static back.ecommerce.constant.PageConstant.*;
+
+import java.util.Map;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import back.ecommerce.domain.product.Category;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+@Getter
+public class ProductSearchCondition {
+
+	private final Category category;
+	private final String name;
+	private final String branName;
+	private final Long minPrice;
+	private final Long MaxPrice;
+	private final ProductSortCondition sortCondition;
+	private final Pageable pageable;
+
+	public static ProductSearchCondition fromQueryParameter(Category category, Map<String, String> params) {
+		return new ProductSearchCondition(
+			category,
+			params.get("name"),
+			params.get("brandName"),
+			Long.parseLong(params.get("minPrice")),
+			Long.parseLong(params.get("maxPrice")),
+			ProductSortCondition.createWithSortQuery((params.get("sort"))),
+			PageRequest.of(Integer.parseInt(params.get("page")), DEFAULT_PAGE_SIZE)
+		);
+	}
+}
