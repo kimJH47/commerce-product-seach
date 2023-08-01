2da053c1bd912617dda891a611792d90d5848223
kimJaeHyeon <kmr2644@gmail.com>
2023-08-01 17:19:12 +0900

    feat: 상품상세 검색 api 구현

--- src/main/java/back/ecommerce/controller/ProductSearchController.java
+++ src/main/java/back/ecommerce/controller/ProductSearchController.java
@@ -1,12 +1,16 @@
 package back.ecommerce.controller;
 
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import back.ecommerce.domain.product.Category;
+import back.ecommerce.dto.ProductSearchCondition;
 import back.ecommerce.dto.response.Response;
 import back.ecommerce.service.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -22,5 +26,14 @@ public class ProductSearchController {
 	public ResponseEntity<Response> findProductWithCategory(@PathVariable String value) {
 		return Response.createSuccessResponse("상품이 성공적으로 조회 되었습니다."
 			, productService.findWithCategoryAndPagination(Category.fromString(value)));
+	}
+
+	@GetMapping("/categories/{category}/detail")
+	public ResponseEntity<Response> findProductWithPagination(@PathVariable String category,
+		@RequestParam Map<String, String> params) {
+		ProductSearchCondition productSearchCondition = ProductSearchCondition.fromQueryParameter(
+			Category.fromString(category), params);
+		return Response.createSuccessResponse("상품이 성공적으로 조회 되었습니다.",
+			productService.findWithSearchCondition(productSearchCondition));
 	}
 }
