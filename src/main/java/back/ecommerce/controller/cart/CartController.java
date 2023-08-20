7d9ba59b960b396cc7864d695919c007144f714d
kimJaeHyeon <kmr2644@gmail.com>
2023-08-20 19:28:32 +0900

    refactor: 장바구니 조회시 토큰 이메일만 요청으로 받는것으로 api 스펙변경

--- src/main/java/back/ecommerce/controller/cart/CartController.java
+++ src/main/java/back/ecommerce/controller/cart/CartController.java
@@ -1,20 +1,17 @@
 package back.ecommerce.controller.cart;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Email;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import back.ecommerce.auth.annotaion.UserEmail;
 import back.ecommerce.dto.request.cart.AddCartRequest;
 import back.ecommerce.dto.response.common.Response;
-import back.ecommerce.exception.TokenHasInvalidException;
 import back.ecommerce.service.cart.CartService;
 import lombok.RequiredArgsConstructor;
 
@@ -34,12 +31,8 @@ public class CartController {
 
 	@GetMapping
 	public ResponseEntity<Response> findByEmail(
-		@UserEmail String tokenEmail,
-		@RequestParam("email") @Valid @Email String email) {
-		if (!tokenEmail.equals(email)) {
-			throw new TokenHasInvalidException("토큰정보와 요청정보가 일치하지 않습니다.");
-		}
+		@UserEmail String tokenEmail) {
 		return Response.createSuccessResponse("장바구니가 성곡적으로 조회 되었습니다.",
-			cartService.findCartByUserEmail(email));
+			cartService.findCartByUserEmail(tokenEmail));
 	}
 }
