50bd7b8295ca29231260ed2d59f275ee8f2d032d
kimJaeHyeon <kmr2644@gmail.com>
2023-09-03 14:51:14 +0900

    refactor: 카테고리 생성메서드 직관적이게 변경 및 이름 축약

--- src/main/java/back/ecommerce/domain/product/Category.java
+++ src/main/java/back/ecommerce/domain/product/Category.java
@@ -1,16 +1,16 @@
 package back.ecommerce.domain.product;
 
-import java.util.Arrays;
+import back.ecommerce.exception.InvalidCategoryNameException;
 
 public enum Category {
 	TOP, OUTER, PANTS, ONEPIECE, SKIRT, SNEAKERS, SHOES, HEAD_WEAR, ACCESSORY;
 
-	public static Category fromString(String value) {
-		return Arrays.stream(Category.values())
-			.limit(1)
-			.filter(category -> category.toString().equalsIgnoreCase(value))
-			.findAny()
-			.orElseThrow(() -> new IllegalArgumentException("일치하는 카테고리가 없습니다."));
+	public static Category from(String value) {
+		try {
+			return Category.valueOf(value.toUpperCase());
+		} catch (NullPointerException |  IllegalArgumentException e) {
+			throw new InvalidCategoryNameException("유효하지 않은 카테고리명 입니다.");
+		}
 	}
-
 }
+
