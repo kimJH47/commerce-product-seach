0d78cb84004084af0413ed0d26b1de8aa3c5725d
kimJaeHyeon <kmr2644@gmail.com>
2023-09-09 16:37:02 +0900

    fix: aop 파라미터 변경

--- src/main/java/back/ecommerce/aop/LoggingAspect.java
+++ src/main/java/back/ecommerce/aop/LoggingAspect.java
@@ -1,7 +1,5 @@
 package back.ecommerce.aop;
 
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.aspectj.lang.annotation.AfterReturning;
@@ -11,6 +9,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import back.ecommerce.controller.common.CustomLogger;
+import back.ecommerce.dto.request.product.ProductSearchConditionRequest;
 
 @Aspect
 public class LoggingAspect {
@@ -18,16 +17,17 @@ public class LoggingAspect {
 	private static final String LOG_FORMAT = "[INFO] request: {} {} | userEmail : {} | category: {} | searchCondition:{}";
 	private static final String DEFAULT_ANONYMOUS_NAME = "NONE";
 	private final CustomLogger logger;
+
 	public LoggingAspect(CustomLogger customLogger) {
 		this.logger = customLogger;
 	}
 
-	@AfterReturning(value = "@annotation(back.ecommerce.aop.annotation.Logging) && args(category,searchCondition)", argNames = "category,searchCondition")
-		public void requestLogging(String category, Map<String,String> searchCondition){
+	@AfterReturning(value = "@annotation(back.ecommerce.aop.annotation.Logging) && args(request)")
+	public void requestLogging(ProductSearchConditionRequest request) {
 		ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes)RequestContextHolder.currentRequestAttributes();
-		HttpServletRequest request = servletRequestAttributes.getRequest();
+		HttpServletRequest servletRequest = servletRequestAttributes.getRequest();
 		RequestContextHolder.getRequestAttributes();
-		logger.log(Level.INFO, LOG_FORMAT, request.getRequestURI(), request.getMethod(),
-			DEFAULT_ANONYMOUS_NAME, category, searchCondition.entrySet());
+		logger.log(Level.INFO, LOG_FORMAT, servletRequest.getRequestURI(), servletRequest.getMethod(),
+			DEFAULT_ANONYMOUS_NAME, request.getCategory(), request.getParameters().entrySet());
 	}
 }
