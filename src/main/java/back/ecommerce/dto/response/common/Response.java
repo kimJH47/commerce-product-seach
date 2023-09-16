07f350633973c19c35aae0b024363b65ea01ea87
kimJaeHyeon <kmr2644@gmail.com>
2023-09-16 20:03:15 +0900

    feat: 실패 response 생성 메서드 구현

--- src/main/java/back/ecommerce/dto/response/common/Response.java
+++ src/main/java/back/ecommerce/dto/response/common/Response.java
@@ -3,6 +3,7 @@ package back.ecommerce.dto.response.common;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
+import back.ecommerce.exception.CustomException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,17 +15,19 @@ public class Response {
 	private String message;
 
 	public static <T> ResponseEntity<Response> createSuccessResponse(String message, T entity) {
-		return ResponseEntity.ok()
-			.body(new SuccessResponse<>(message, entity));
+		return ResponseEntity.ok().body(new SuccessResponse<>(message, entity));
 	}
 
 	public static ResponseEntity<Response> createBadRequest(String message, MethodArgumentNotValidException e) {
-		return ResponseEntity.badRequest()
-			.body(new FailedResponse(message, e));
+		return ResponseEntity.badRequest().body(new FailedResponse(message, e));
 	}
 
 	public static ResponseEntity<Response> createBadRequest(String message, String field, String reason) {
-		return ResponseEntity.badRequest()
-			.body(new FailedResponse(message, field, reason));
+		return ResponseEntity.badRequest().body(new FailedResponse(message, field, reason));
+	}
+
+	public static ResponseEntity<Response> Failed(String message, CustomException exception) {
+		return ResponseEntity.status(exception.getStatusCode())
+			.body(new FailedResponse(message, exception.getReasonField(), exception.getDescription()));
 	}
 }
