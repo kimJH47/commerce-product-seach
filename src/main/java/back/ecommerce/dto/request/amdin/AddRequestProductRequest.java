b63e1df8f740297a40eafb59cb0adafd8cac9a94
kimJaeHyeon <kmr2644@gmail.com>
2023-09-21 19:31:51 +0900

    fix: response 필드명 변경 및 request price 검증 어노테이션 메시지 추가

--- src/main/java/back/ecommerce/dto/request/amdin/AddRequestProductRequest.java
+++ src/main/java/back/ecommerce/dto/request/amdin/AddRequestProductRequest.java
@@ -21,7 +21,7 @@ public class AddRequestProductRequest {
 	private String name;
 	@NotBlank(message = "브랜드의 이름은 필수적으로 필요 합니다.")
 	private String brandName;
-	@NotNull
+	@NotNull(message = "가격정보는 필수적으로 필요 합니다.")
 	@Min(value = 1, message = "상품의 가격은 최소 1 이상 이여야 합니다.")
 	private Long price;
 	@NotBlank(message = "상품의 카테고리는 필수적으로 필요 합니다.")
