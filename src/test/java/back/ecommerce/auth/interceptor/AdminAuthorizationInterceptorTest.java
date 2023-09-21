d47021960820e95bb45197b6308e2b7b7acec0cb
kimJaeHyeon <kmr2644@gmail.com>
2023-09-22 03:58:05 +0900

    test: 어드민 인터셉터 단위 테스트 작성

---
+++ src/test/java/back/ecommerce/auth/interceptor/AdminAuthorizationInterceptorTest.java
@@ -0,0 +1,74 @@
+package back.ecommerce.auth.interceptor;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import back.ecommerce.domain.admin.Admin;
+import back.ecommerce.exception.AuthenticationException;
+import back.ecommerce.repository.admin.AdminRepository;
+
+@ExtendWith(MockitoExtension.class)
+class AdminAuthorizationInterceptorTest {
+
+	AdminAuthorizationInterceptor adminAuthorizationInterceptor;
+	@Mock
+	AdminRepository adminRepository;
+	@Mock
+	HttpServletRequest request;
+	@Mock
+	HttpServletResponse response;
+	@Mock
+	Object handle;
+
+	@BeforeEach
+	void setUp() {
+		adminAuthorizationInterceptor = new AdminAuthorizationInterceptor(adminRepository);
+	}
+
+	@Test
+	@DisplayName("어드민이 유효할시 정상적으로 통과되어야함")
+	void handle() throws Exception {
+		//given
+		given(request.getAttribute(anyString())).willReturn("email@email.com");
+		given(adminRepository.findByEmail(anyString()))
+			.willReturn(Optional.of(new Admin(15L, "email@email.com", "password")));
+
+		//when
+		boolean actual = adminAuthorizationInterceptor.preHandle(request, response, handle);
+
+		//then
+		assertThat(actual).isTrue();
+		then(adminRepository).should(times(1)).findByEmail(anyString());
+		then(request).should(times(1)).getAttribute(anyString());
+	}
+
+	@Test
+	@DisplayName("어드민이 유효 하지 않을 시 AuthenticationException 을 던저야함")
+	void handle_admin_not_found() {
+		//given
+		given(request.getAttribute(anyString())).willReturn("email@email.com");
+		given(adminRepository.findByEmail(anyString()))
+			.willReturn(Optional.empty());
+
+		//expect
+		assertThatThrownBy(() -> adminAuthorizationInterceptor.preHandle(request, response, handle))
+			.isInstanceOf(AuthenticationException.class)
+			.hasMessage("권한이 없습니다.");
+
+		then(adminRepository).should(times(1)).findByEmail(anyString());
+		then(request).should(times(1)).getAttribute(anyString());
+
+	}
+}
