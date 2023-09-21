87b6bae28dcb7141c345420f101c74df431ae0a1
kimJaeHyeon <kmr2644@gmail.com>
2023-09-22 03:21:13 +0900

    feat: 검증어노테이션 추가

--- src/main/java/back/ecommerce/dto/request/amdin/AddProductRequest.java
+++ src/main/java/back/ecommerce/dto/request/amdin/AddProductRequest.java
@@ -1,5 +1,9 @@
 package back.ecommerce.dto.request.amdin;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import back.ecommerce.domain.product.ApprovalStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,7 +13,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AddProductRequest {
+	@NotNull(message = "등록요청 상품 아아디는 필수적으로 필요합니다.")
 	private Long requestId;
+	@NotNull(message = "승인상태는 필수적로 필요합니다.")
 	private ApprovalStatus approvalStatus;
+	@NotBlank(message = "이메일은 필수적으로 필요합니다.")
+	@Email(message = "옳바른 이메일 형식이 아닙니다.")
 	private String email;
 }
