c38af9d035342808b0be78b381708c3bc8af0efb
kimJaeHyeon <kmr2644@gmail.com>
2023-10-21 16:41:39 +0900

    feat: swagger-ui config 구현

---
+++ src/main/java/back/ecommerce/config/swagger/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package back.ecommerce.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.components(new Components())
+			.info(apiInfo());
+	}
+
+	private Info apiInfo() {
+		return new Info()
+			.title("Ecommerce search service Spring docs")
+			.description("API docs")
+			.version("0.0.1");
+	}
+}
