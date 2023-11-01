b19e7e5564101a91e2e22521c2c9b8a67e19aa30
kimJaeHyeon <kmr2644@gmail.com>
2023-11-01 21:32:48 +0900

    feat: 생성메서드 구현

--- src/main/java/back/ecommerce/order/entity/OrderItem.java
+++ src/main/java/back/ecommerce/order/entity/OrderItem.java
@@ -9,10 +9,18 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import back.ecommerce.api.payment.OrderProductDto;
 import back.ecommerce.common.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "ORDER_ITEM")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class OrderItem extends BaseTimeEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,7 +28,16 @@ public class OrderItem extends BaseTimeEntity {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "order_group_id")
 	private OrderGroup orderGroup;
+	private Long productId;
 	private String name;
 	private Long price;
 	private int quantity;
+
+	public static OrderItem create(OrderProductDto orderProduct) {
+		return new OrderItem(null, null,
+			orderProduct.getProductId(),
+			orderProduct.getName(),
+			orderProduct.getPrice(),
+			orderProduct.getQuantity());
+	}
 }
