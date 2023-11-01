f03b669b13a2b00adc3f96cacb9b0ebd524efdc3
kimJaeHyeon <kmr2644@gmail.com>
2023-11-01 21:37:58 +0900

    feat: 카카오페이 결재 client 구현

---
+++ src/main/java/back/ecommerce/client/KakaoPaymentClient.java
@@ -0,0 +1,39 @@
+package back.ecommerce.client;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import back.ecommerce.api.payment.KakakoPaymentReadyResponse;
+import back.ecommerce.api.payment.KakaoReadyPaymentResult;
+import back.ecommerce.api.payment.KakaoPaymentReadyRequest;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class KakaoPaymentClient {
+
+	private final String adminKey;
+	private final String cid;
+	private final String approvalUrl;
+	private final String cancelUrl;
+	private final String failUrl;
+	private final WebClient webClient;
+
+	public KakaoReadyPaymentResult ready(String userEmail, String orderCode, Long totalPrice, String name,
+		Integer quantity) {
+		KakaoPaymentReadyRequest request = new KakaoPaymentReadyRequest(
+			cid, orderCode, userEmail, name, quantity, totalPrice, 0L, approvalUrl, cancelUrl, failUrl
+		);
+		KakakoPaymentReadyResponse response = webClient.post()
+			.header(HttpHeaders.AUTHORIZATION, adminKey)
+			.body(BodyInserters.fromFormData(request.toMap()))
+			.retrieve()
+			.bodyToMono(KakakoPaymentReadyResponse.class)
+			.block();
+		assert response != null;
+		return new KakaoReadyPaymentResult(response.getNext_redirect_app_url(),
+			response.getNext_redirect_mobile_url(), response.getNext_redirect_pc_url(),
+			cid, response.getTid(), orderCode);
+	}
+
+}
---
+++ src/main/java/back/ecommerce/client/config/PaymentClientConfig.java
@@ -0,0 +1,43 @@
+package back.ecommerce.client.config;
+
+import static com.amazonaws.services.s3.Headers.*;
+import static org.springframework.http.MediaType.*;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import back.ecommerce.client.KakaoPaymentClient;
+
+@Configuration
+public class PaymentClientConfig {
+	@Value("kakao.payment.adminKey")
+	private String adminKey;
+	@Value("kakao.payment.cid")
+	private String cid;
+	@Value("kakao.payment.approvalUrl")
+	private String approvalUrl;
+	@Value("kakao.payment.cancelUrl")
+	private String cancelUrl;
+	@Value("kakao.payment.failUrl")
+	private String failUrl;
+	@Bean
+	public KakaoPaymentClient kakaoPaymentClient() {
+		return new KakaoPaymentClient(
+			adminKey,
+			cid,
+			approvalUrl,
+			cancelUrl,
+			failUrl
+			, webClient());
+	}
+
+	@Bean
+	public WebClient webClient() {
+		return WebClient.builder()
+			.baseUrl("https://kapi.kakao.com/v1/payment/ready")
+			.defaultHeader(CONTENT_TYPE, APPLICATION_FORM_URLENCODED_VALUE)
+			.build();
+	}
+}
