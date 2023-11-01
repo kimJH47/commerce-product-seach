76236650c89621b63becd79a7cf5e724d47e1998
kimJaeHyeon <kmr2644@gmail.com>
2023-11-01 21:34:54 +0900

    feat: 주문생성 구현

---
+++ src/main/java/back/ecommerce/api/payment/OrderProductDto.java
@@ -0,0 +1,22 @@
+package back.ecommerce.api.payment;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+public class OrderProductDto {
+	@NotNull(message = "상품의 아이디는 필수적으로 필요합니다.")
+	private Long productId;
+	private String name;
+	@Min(value = 1,message = "상품의 갯수는 최소 1개 이상이여야 합니다.")
+	private Integer quantity;
+	@Min(value = 1,message = "상품의 갯수는 최소 1개 이상이여야 합니다.")
+	@NotNull(message = "가격은 필수적으로 필요 합니다.")
+	private Long price;
+}
