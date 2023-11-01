535e72fc9dfaf039a12af700aef7424543839539
kimJaeHyeon <kmr2644@gmail.com>
2023-11-01 21:32:48 +0900

    feat: 생성메서드 구현

--- src/main/java/back/ecommerce/order/entity/OrderGroup.java
+++ src/main/java/back/ecommerce/order/entity/OrderGroup.java
@@ -1,6 +1,7 @@
 package back.ecommerce.order.entity;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -12,10 +13,18 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import back.ecommerce.api.payment.OrderProductDto;
 import back.ecommerce.common.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "ORDER_GROUP")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class OrderGroup extends BaseTimeEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,6 +36,39 @@ public class OrderGroup extends BaseTimeEntity {
 	private int quantity;
 	@Enumerated(EnumType.STRING)
 	private OrderStatus orderStatus;
-	@OneToMany(mappedBy = "orderGroup",cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "orderGroup", cascade = CascadeType.ALL)
 	private List<OrderItem> orderItems;
+
+	public String getOrderCode() {
+		return orderCode;
+	}
+
+	public Long getTotalPrice() {
+		return totalPrice;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public static OrderGroup createWithOrderItems(String userEmail, Long totalPrice, String name, String orderCode,
+		List<OrderProductDto> orderProducts) {
+		List<OrderItem> items = orderProducts.stream()
+			.map(OrderItem::create)
+			.collect(Collectors.toList());
+		return OrderGroup.builder()
+			.orderCode(orderCode)
+			.name(name)
+			.userEmail(userEmail)
+			.totalPrice(totalPrice)
+			.quantity(items.size())
+			.orderStatus(OrderStatus.PAYMENT_READY)
+			.orderItems(items)
+			.build();
+	}
+
 }
--- src/main/java/back/ecommerce/payment/entity/Payment.java
+++ src/main/java/back/ecommerce/payment/entity/Payment.java
@@ -6,8 +6,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import back.ecommerce.common.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Payment extends BaseTimeEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +24,9 @@ public class Payment extends BaseTimeEntity {
 	private PaymentStatus paymentStatus;
 	private Long totalPrice;
 	private Long taxFreePrice;
+
+	public static Payment createReadyPayment(String transactionId, String cid, String orderCode, String userEmail,
+		Long totalPrice) {
+		return new Payment(null, transactionId, cid, userEmail, orderCode, PaymentStatus.READY, totalPrice, 0L);
+	}
 }
