6fd1e6cf5f3315d67c8ed3bc2ed4b83815246eee
kimJaeHyeon <kmr2644@gmail.com>
2023-11-03 11:53:18 +0900

    refactor: OrderItems 생성시 OrderGroup 넣어주는 형식으로 변경

--- src/main/java/back/ecommerce/order/service/OrderService.java
+++ src/main/java/back/ecommerce/order/service/OrderService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import back.ecommerce.api.payment.OrderProductDto;
 import back.ecommerce.common.generator.UuidGenerator;
@@ -25,10 +26,11 @@ public class OrderService {
 	private final UserRepository userRepository;
 	private final UuidGenerator uuidGenerator;
 
+	@Transactional
 	public OrderGroupDto createOrder(String userEmail, Long totalPrice, List<OrderProductDto> orderProducts) {
 		validateUserEmail(userEmail);
 		List<Product> products = findByIds(orderProducts);
-		validateOrder(orderProducts, products, totalPrice);
+		validateOrder(orderProducts, products);
 		OrderGroup orderGroup = orderGroupRepository.save(createOrderGroup(userEmail, totalPrice, orderProducts));
 		return OrderGroupDto.create(orderGroup);
 	}
@@ -45,28 +47,36 @@ public class OrderService {
 		}
 	}
 
-	private void validateOrder(List<OrderProductDto> orderProducts, List<Product> products, Long totalPrice) {
+	private void validateOrder(List<OrderProductDto> orderProducts, List<Product> products) {
 		if (orderProducts.size() != products.size()) {
 			throw new CustomException(ErrorCode.INVALID_ORDER_ARGUMENT);
 		}
 		if (products.stream()
 			.mapToLong(Product::getPrice)
-			.sum() != totalPrice) {
+			.sum() != orderProducts.stream()
+			.mapToLong(OrderProductDto::getPrice)
+			.sum()) {
 			throw new CustomException(ErrorCode.INVALID_TOTAL_PRICE);
 		}
 	}
 
 	private OrderGroup createOrderGroup(String email, Long totalPrice, List<OrderProductDto> products) {
 		String orderCode = uuidGenerator.create();
-		String name = createShortenName(products);
-		return OrderGroup.createWithOrderItems(email, totalPrice, name, orderCode, products);
+		int quantity = calculateTotalQuantity(products);
+		String name = createShortenName(products, quantity);
+		return OrderGroup.createWithOrderItems(email, totalPrice, quantity, name, orderCode, products);
 	}
 
-	private String createShortenName(List<OrderProductDto> products) {
+	private String createShortenName(List<OrderProductDto> products, int quantity) {
 		if (products.size() == 1) {
 			return products.get(0).getName();
 		}
-		return String.format("%s 등 총 %s개의 상품", products.get(0).getName(), products.size());
+		return String.format("%s 등 총 %s개의 상품", products.get(0).getName(), quantity);
 	}
 
+	private int calculateTotalQuantity(List<OrderProductDto> products) {
+		return products.stream()
+			.mapToInt(OrderProductDto::getQuantity)
+			.sum();
+	}
 }
