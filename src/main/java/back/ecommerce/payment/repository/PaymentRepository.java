80b14631a2a1674e922793ba0912bb8f7c6c2d41
kimJaeHyeon <kmr2644@gmail.com>
2023-11-03 16:08:54 +0900

    feat: 결제정보 조회 구현

--- src/main/java/back/ecommerce/payment/repository/PaymentRepository.java
+++ src/main/java/back/ecommerce/payment/repository/PaymentRepository.java
@@ -1,8 +1,11 @@
 package back.ecommerce.payment.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import back.ecommerce.payment.entity.Payment;
 
 public interface PaymentRepository extends JpaRepository<Payment,Long> {
+	Optional<Payment> findByOrderCode(String orderCode);
 }
--- src/main/java/back/ecommerce/payment/service/PaymentService.java
+++ src/main/java/back/ecommerce/payment/service/PaymentService.java
@@ -1,7 +1,10 @@
 package back.ecommerce.payment.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import back.ecommerce.exception.CustomException;
+import back.ecommerce.exception.ErrorCode;
 import back.ecommerce.payment.entity.Payment;
 import back.ecommerce.payment.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +14,23 @@ import lombok.RequiredArgsConstructor;
 public class PaymentService {
 	private final PaymentRepository paymentRepository;
 
+	@Transactional
 	public void createReadyPayment(String transactionId, String cid, String orderCode, String userEmail,
 		Long totalPrice) {
 		paymentRepository.save(Payment.createReadyPayment(transactionId, cid, orderCode, userEmail, totalPrice));
+	}
+
+	@Transactional(readOnly = true)
+	public PaymentDto findByOrderCode(String orderCode) {
+		Payment payment = paymentRepository.findByOrderCode(orderCode)
+			.orElseThrow(() -> new CustomException(ErrorCode.PAYMENT_NOT_FOUND));
+		validatePayment(payment);
+		return new PaymentDto(payment.getTransactionId(), payment.getOrderCode(), payment.getUserEmail());
+	}
+
+	private void validatePayment(Payment payment) {
+		if (payment.isNotReady()) {
+			throw new CustomException(ErrorCode.ALREADY_PROCESS_PAYMENT);
+		}
 	}
 }
