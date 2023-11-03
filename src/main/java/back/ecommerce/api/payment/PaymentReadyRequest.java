abdee0aa558037dd22bde65fad9637de98632a1a
kimJaeHyeon <kmr2644@gmail.com>
2023-11-03 11:55:26 +0900

    refactor: 검증 어노테이션 추가

--- src/main/java/back/ecommerce/api/payment/PaymentReadyRequest.java
+++ src/main/java/back/ecommerce/api/payment/PaymentReadyRequest.java
@@ -2,14 +2,24 @@ package back.ecommerce.api.payment;
 
 import java.util.List;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 public class PaymentReadyRequest {
+	@NotBlank(message = "이메일은 필수적으로 필요합니다.")
+	@Email(message = "옳바른 이메일 형식이 아닙니다.")
 	private String email;
+	@Min(value = 1, message = "상품의 전체갯수는 최소 1개 이상이여야 합니다.")
 	private Long totalCount;
+	@Min(value = 1, message = "상품의 전체금액은 최소 1 이상이여야 합니다.")
 	private Long totalPrice;
 	private List<OrderProductDto> orderProducts;
 }
