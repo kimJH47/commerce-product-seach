145f08082e821e0532f7cdd01f3095d444ac572f
kimJaeHyeon <kmr2644@gmail.com>
2023-11-03 11:55:08 +0900

    refactor: 인증헤더 제외, 메서드 분리

--- src/main/java/back/ecommerce/client/KakaoPaymentClient.java
+++ src/main/java/back/ecommerce/client/KakaoPaymentClient.java
@@ -1,18 +1,16 @@
 package back.ecommerce.client;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import back.ecommerce.api.payment.KakakoPaymentReadyResponse;
-import back.ecommerce.api.payment.KakaoReadyPaymentResult;
 import back.ecommerce.api.payment.KakaoPaymentReadyRequest;
+import back.ecommerce.api.payment.KakaoReadyPaymentResult;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class KakaoPaymentClient {
 
-	private final String adminKey;
 	private final String cid;
 	private final String approvalUrl;
 	private final String cancelUrl;
@@ -21,20 +19,28 @@ public class KakaoPaymentClient {
 
 	public KakaoReadyPaymentResult ready(String userEmail, String orderCode, Long totalPrice, String name,
 		Integer quantity) {
-		KakaoPaymentReadyRequest request = new KakaoPaymentReadyRequest(
-			cid, orderCode, userEmail, name, quantity, totalPrice, 0L, approvalUrl, cancelUrl, failUrl
-		);
-		KakakoPaymentReadyResponse response = webClient.post()
-			.uri("/ready")
-			.header(HttpHeaders.AUTHORIZATION, adminKey)
-			.body(BodyInserters.fromFormData(request.toMap()))
-			.retrieve()
-			.bodyToMono(KakakoPaymentReadyResponse.class)
-			.block();
+		KakaoPaymentReadyRequest request = createKakaoPaymentReadyRequest(userEmail, orderCode, totalPrice, name,
+			quantity);
+		KakakoPaymentReadyResponse response = getKakaoPaymentReadyResponse(request);
 		assert response != null;
 		return new KakaoReadyPaymentResult(response.getNext_redirect_app_url(),
 			response.getNext_redirect_mobile_url(), response.getNext_redirect_pc_url(),
 			cid, response.getTid(), orderCode);
 	}
 
+	private KakaoPaymentReadyRequest createKakaoPaymentReadyRequest(String userEmail, String orderCode, Long totalPrice,
+		String name, Integer quantity) {
+		return new KakaoPaymentReadyRequest(
+			cid, orderCode, userEmail, name, quantity, totalPrice, 0L, approvalUrl, cancelUrl, failUrl
+		);
+	}
+
+	private KakakoPaymentReadyResponse getKakaoPaymentReadyResponse(KakaoPaymentReadyRequest request) {
+		return webClient.post()
+			.uri("/ready")
+			.body(BodyInserters.fromFormData(request.toMap()))
+			.retrieve()
+			.bodyToMono(KakakoPaymentReadyResponse.class)
+			.block();
+	}
 }
