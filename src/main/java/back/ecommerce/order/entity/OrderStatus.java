4badbac8db2a159a98ad422d7438d91c0f1357d0
kimJaeHyeon <kmr2644@gmail.com>
2023-11-05 22:42:20 +0900

    feat: 주문상태 추가, 취소검증 메서드 구현

--- src/main/java/back/ecommerce/order/entity/OrderStatus.java
+++ src/main/java/back/ecommerce/order/entity/OrderStatus.java
@@ -7,10 +7,17 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum OrderStatus {
 	ORDER_FAILED("주문실패"),
+	ORDER_CANCEL("주문 취소"),
 	PAYMENT_READY("결제준비"),
+	PAYMENT_APPROVAL("결제 승인"),
+	DELIVERY_READY("배송 준비"),
 	DELIVERY("배송중"),
 	DELIVERY_SUCCEED("배송 성공"),
 	PAYMENT_FAILED("결제 실패");
 
 	private final String description;
+
+	public static boolean cancellable(OrderStatus orderStatus) {
+		return orderStatus == PAYMENT_READY || orderStatus == PAYMENT_APPROVAL || orderStatus == DELIVERY_READY;
+	}
 }
