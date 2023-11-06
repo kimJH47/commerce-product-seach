0ca7301fa1acdb4fb482660818041e1686412e1e
kimJaeHyeon <kmr2644@gmail.com>
2023-11-06 17:53:47 +0900

    ci test

---
+++ src/test/java/back/ecommerce/CI.java
@@ -0,0 +1,18 @@
+package back.ecommerce;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CI {
+	@Test
+	@DisplayName("")
+	void CI() {
+	    //given
+		Assertions.assertThat(true).isFalse();
+	    //when
+
+	    //then
+
+	}
+}
