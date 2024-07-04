e41fe3d582e1149695398d3225ab0d1fca66abc0
kimJaeHyeon <kmr2644@gmail.com>
2024-07-04 19:38:09 +0900

    chore: import 문 정리

--- src/main/java/back/ecommerce/config/jpa/QueryDslConfig.java
+++ src/main/java/back/ecommerce/config/jpa/QueryDslConfig.java
@@ -1,12 +1,12 @@
 package back.ecommerce.config.jpa;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
 @Configuration
 public class QueryDslConfig {
