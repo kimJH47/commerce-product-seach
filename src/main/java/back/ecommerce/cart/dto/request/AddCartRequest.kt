6a8df5d154eb7aed207155e3edad3363d903a7d6
kimJaeHyeon <kmr2644@gmail.com>
2024-07-05 22:52:20 +0900

    refactor: request, response 재정의

--- src/main/java/back/ecommerce/cart/dto/request/AddCartRequest.kt
+++ src/main/java/back/ecommerce/cart/dto/request/AddCartRequest.kt
@@ -1,24 +1,17 @@
-package back.ecommerce.cart.dto.request;
+package back.ecommerce.cart.dto.request
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+data class AddCartRequest(
+    @field:NotBlank(message = "이메일은 필수적으로 필요합니다.")
+    @field:Email(message = "옳바른 이메일 형식이 아닙니다.")
+    val email: String,
+    @field:NotNull(message = "상품의 아이디는 필수적으로 필요합니다.")
+    val productId: Long,
+    @field:Min(value = 1, message = "상품의 갯수는 최소 1개 이상이여야 합니다.")
+    val quantity: Int
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-public class AddCartRequest {
-	@NotBlank(message = "이메일은 필수적으로 필요합니다.")
-	@Email(message = "옳바른 이메일 형식이 아닙니다.")
-	private String email;
-	@NotNull(message = "상품의 아이디는 필수적으로 필요합니다.")
-	private Long productId;
-	@Min(value = 1,message = "상품의 갯수는 최소 1개 이상이여야 합니다.")
-	private Integer quantity;
-
-}
+)
--- src/main/java/back/ecommerce/cart/dto/request/DeleteCartRequest.kt
+++ src/main/java/back/ecommerce/cart/dto/request/DeleteCartRequest.kt
@@ -1,11 +1,14 @@
-package back.ecommerce.cart.dto.request;
+package back.ecommerce.cart.dto.request
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 
-@NoArgsConstructor
-@Getter
-public class DeleteCartRequest {
-	private String email;
-	private Long cartId;
+data class DeleteCartRequest(
+    @field:NotBlank(message = "이메일은 필수적으로 필요합니다.")
+    @field:Email(message = "옳바른 이메일 형식이 아닙니다.")
+    val email: String,
+    @field:NotNull(message = "카트 상품의 아이디는 필수적으로 필요합니다.")
+    val cartId: String,
+) {
 }
