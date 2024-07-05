5855d70e7d78024df72417fcc179d35db237bd95
kimJaeHyeon <kmr2644@gmail.com>
2024-07-05 22:52:44 +0900

    fix: requestId 타입변경

--- src/main/java/back/ecommerce/cart/dto/request/DeleteCartRequest.kt
+++ src/main/java/back/ecommerce/cart/dto/request/DeleteCartRequest.kt
@@ -9,6 +9,6 @@ data class DeleteCartRequest(
     @field:Email(message = "옳바른 이메일 형식이 아닙니다.")
     val email: String,
     @field:NotNull(message = "카트 상품의 아이디는 필수적으로 필요합니다.")
-    val cartId: String,
+    val cartId: Long,
 ) {
 }
