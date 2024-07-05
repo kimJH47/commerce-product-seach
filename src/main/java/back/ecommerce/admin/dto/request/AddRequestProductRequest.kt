11061aa47e37ae845631c668abe582681bd48b6b
kimJaeHyeon <kmr2644@gmail.com>
2024-07-05 14:42:47 +0900

    refactor: request 재정의

--- src/main/java/back/ecommerce/admin/dto/request/AddRequestProductRequest.kt
+++ src/main/java/back/ecommerce/admin/dto/request/AddRequestProductRequest.kt
@@ -1,30 +1,21 @@
-package back.ecommerce.admin.dto.request;
+package back.ecommerce.admin.dto.request
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-public class AddRequestProductRequest {
-
-	@NotBlank(message = "이메일은 필수적으로 필요합니다.")
-	@Email(message = "옳바른 이메일 형식이 아닙니다.")
-	private String email;
-	@NotBlank(message = "상품의 이름은 필수적으로 필요 합니다.")
-	private String name;
-	@NotBlank(message = "브랜드의 이름은 필수적으로 필요 합니다.")
-	private String brandName;
-	@NotNull(message = "가격정보는 필수적으로 필요 합니다.")
-	@Min(value = 1, message = "상품의 가격은 최소 1 이상 이여야 합니다.")
-	private Long price;
-	@NotBlank(message = "상품의 카테고리는 필수적으로 필요 합니다.")
-	private String category;
-
-}
+data class AddRequestProductRequest(
+    @field:NotBlank(message = "이메일은 필수적으로 필요합니다.")
+    @field:Email(message = "옳바른 이메일 형식이 아닙니다.")
+    var email: String,
+    @field:NotBlank(message = "상품의 이름은 필수적으로 필요 합니다.")
+    val name: String,
+    @field:NotBlank(message = "브랜드의 이름은 필수적으로 필요 합니다.")
+    val brandName: String,
+    @field:NotNull(message = "가격정보는 필수적으로 필요 합니다.")
+    @field:Min(value = 1, message = "상품의 가격은 최소 1 이상 이여야 합니다.")
+    val price: Long?,
+    @field:NotBlank(message = "상품의 카테고리는 필수적으로 필요 합니다.")
+    val category: String
+)
--- src/main/java/back/ecommerce/admin/dto/request/UpdateApprovalRequest.kt
+++ src/main/java/back/ecommerce/admin/dto/request/UpdateApprovalRequest.kt
@@ -1,23 +1,16 @@
-package back.ecommerce.admin.dto.request;
+package back.ecommerce.admin.dto.request
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import back.ecommerce.product.entity.ApprovalStatus
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 
-import back.ecommerce.product.entity.ApprovalStatus;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class UpdateApprovalRequest {
-	@NotNull(message = "등록요청 상품 아아디는 필수적으로 필요합니다.")
-	private Long requestId;
-	@NotNull(message = "승인상태는 필수적로 필요합니다.")
-	private ApprovalStatus approvalStatus;
-	@NotBlank(message = "이메일은 필수적으로 필요합니다.")
-	@Email(message = "옳바른 이메일 형식이 아닙니다.")
-	private String email;
-}
+data class UpdateApprovalRequest(
+    @field:NotNull(message = "등록요청 상품 아아디는 필수적으로 필요합니다.")
+    var requestId: Long?,
+    @field:NotNull(message = "승인상태는 필수적로 필요합니다.")
+    val approvalStatus: ApprovalStatus,
+    @field:NotBlank(message = "이메일은 필수적으로 필요합니다.")
+    @field:Email(message = "옳바른 이메일 형식이 아닙니다.")
+    val email: String
+)
