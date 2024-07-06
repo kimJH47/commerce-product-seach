c666c3d6943b8de2108fe0d8b6d6dfea1af46ead
kimJaeHyeon <kmr2644@gmail.com>
2024-07-06 13:56:40 +0900

    feat: ProductV2Controller 구현

---
+++ src/main/java/back/ecommerce/api/product/v2/ProductV2Controller.kt
@@ -0,0 +1,43 @@
+package back.ecommerce.api.product.v2
+
+import back.ecommerce.api.dto.Response
+import back.ecommerce.api.resolver.ProductSearchRequestMapping
+import back.ecommerce.common.logging.aop.annotation.Logging
+import back.ecommerce.product.dto.request.ProductSearchConditionRequest
+import back.ecommerce.product.entity.Category
+import back.ecommerce.product.service.ProductService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v2")
+class ProductV2Controller(
+    val productService: ProductService
+) {
+
+    @GetMapping("/categories/{category}")
+    fun getProductsByCategory(@PathVariable("category") category: String): ResponseEntity<Response> {
+        return Response.createSuccessResponse(
+            "상품이 성공적으로 조회 되었습니다.",
+            productService.findWithCategoryAndPagination(Category.from(category))
+        )
+    }
+
+    @Logging
+    @GetMapping("/categories/**/detail")
+    fun findProductWithPagination(@ProductSearchRequestMapping request: ProductSearchConditionRequest)
+            : ResponseEntity<Response> {
+        return Response.createSuccessResponse(
+            "상품이 성공적으로 조회 되었습니다.",
+            productService.findWithSearchCondition(request.category, request.parameters)
+        )
+    }
+
+    @GetMapping("/product/{id}")
+    fun findOne(@PathVariable("id") id: Long): ResponseEntity<Response> {
+        return Response.createSuccessResponse("상품이 성공적으로 조회 되었습니다.", productService.findOne(id))
+    }
+}
