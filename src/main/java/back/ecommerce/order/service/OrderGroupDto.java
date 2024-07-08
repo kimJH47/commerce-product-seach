e0a362831503017d895027938eb28758764bfdf4
kimJaeHyeon <kmr2644@gmail.com>
2024-07-08 20:30:33 +0900

    refactor: record class 로 변경

--- src/main/java/back/ecommerce/order/service/OrderGroupDto.java
+++ src/main/java/back/ecommerce/order/service/OrderGroupDto.java
@@ -1,19 +1,11 @@
 package back.ecommerce.order.service;
 
 import back.ecommerce.order.entity.OrderGroup;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
-@Getter
-public class OrderGroupDto {
-	private final String orderCode;
-	private final String name;
-	private final Integer quantity;
-	private final Long totalPrice;
-
+public record OrderGroupDto(String orderCode, String name, Integer quantity, Long totalPrice) {
 	public static OrderGroupDto create(OrderGroup orderGroup) {
 		return new OrderGroupDto(orderGroup.getOrderCode(), orderGroup.getName(), orderGroup.getQuantity(),
 			orderGroup.getTotalPrice());
 	}
+
 }
