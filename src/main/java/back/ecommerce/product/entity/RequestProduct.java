67d66db80f468fe8f6a006618bf210710bf73e14
kimJaeHyeon <kmr2644@gmail.com>
2024-07-08 20:33:25 +0900

    refactor: getter 명시

--- src/main/java/back/ecommerce/product/entity/RequestProduct.java
+++ src/main/java/back/ecommerce/product/entity/RequestProduct.java
@@ -2,6 +2,9 @@ package back.ecommerce.product.entity;
 
 import static back.ecommerce.product.entity.ApprovalStatus.*;
 
+import back.ecommerce.common.entity.BaseTimeEntity;
+import back.ecommerce.exception.CustomException;
+import back.ecommerce.exception.ErrorCode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -9,20 +12,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
-import back.ecommerce.common.entity.BaseTimeEntity;
-import back.ecommerce.exception.CustomException;
-import back.ecommerce.exception.ErrorCode;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "REQUEST_PRODUCT")
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
 public class RequestProduct extends BaseTimeEntity {
 
 	@Id
@@ -44,12 +41,41 @@ public class RequestProduct extends BaseTimeEntity {
 		this.approvalStatus = approvalStatus;
 	}
 
+	public Product toProduct() {
+		return new Product(null, name, brandName, price, category);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getBrandName() {
+		return brandName;
+	}
+
+	public Long getPrice() {
+		return price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public ApprovalStatus getApprovalStatus() {
+		return approvalStatus;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
 	public static RequestProduct createWithWaitStatus(String name, String brandName, Long price, Category category,
 		String email) {
 		return new RequestProduct(null, name, brandName, price, category, WAIT, email);
 	}
 
-	public Product toProduct() {
-		return new Product(null, name, brandName, price, category);
-	}
 }
