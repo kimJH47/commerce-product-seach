917a0d57442d024966ecb6fb631e6a5874ec6a33
kimJaeHyeon <kmr2644@gmail.com>
2024-07-08 20:50:44 +0900

    refactor: toMap 구현

--- src/main/java/back/ecommerce/auth/dto/response/SignUpDto.kt
+++ src/main/java/back/ecommerce/auth/dto/response/SignUpDto.kt
@@ -3,4 +3,9 @@ package back.ecommerce.auth.dto.response
 data class SignUpDto(
     val email: String,
     val code: String
-)
+) {
+
+    fun toMap(): Map<String, String> {
+        return mapOf("email" to email, "code" to code)
+    }
+}
