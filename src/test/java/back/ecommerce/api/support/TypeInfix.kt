ab2b9d396b997b948405170971e0a1106f46dacc
kimJaeHyeon <kmr2644@gmail.com>
2024-07-08 20:32:28 +0900

    test: Field Type infix 구현

---
+++ src/test/java/back/ecommerce/api/support/TypeInfix.kt
@@ -0,0 +1,27 @@
+package back.ecommerce.api.support
+
+import org.aspectj.weaver.tools.cache.SimpleCacheFactory.path
+import org.springframework.restdocs.payload.FieldDescriptor
+import org.springframework.restdocs.payload.JsonFieldType
+import org.springframework.restdocs.payload.PayloadDocumentation
+
+
+infix fun String.type(docsFieldType: DocsFieldType): Field {
+    val field = createField(this, docsFieldType.type)
+    when (docsFieldType) {
+        is DATE -> field formattedAs "2024-01-01"
+        is DATETIME -> field formattedAs "2024-01-01T00:00:00"
+        else -> {}
+    }
+    return field
+}
+
+private fun createField(value: String, type: JsonFieldType, optional: Boolean = false): Field {
+    val descriptor = PayloadDocumentation.fieldWithPath(value)
+        .type(type)
+        //.attributes(RestDocsUtils.emptyFormat(), RestDocsUtils.emptyDefaultValue())
+        .description("")
+    if (optional) descriptor.optional()
+
+    return Field(descriptor)
+}
