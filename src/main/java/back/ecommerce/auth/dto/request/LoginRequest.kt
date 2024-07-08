c9a4a021b2dbe32d8d0a3c134419d481d7b7b1bc
kimJaeHyeon <kmr2644@gmail.com>
2024-07-08 11:17:02 +0900

    refactor: dto data class 로  재정의

--- src/main/java/back/ecommerce/auth/dto/request/LoginRequest.kt
+++ src/main/java/back/ecommerce/auth/dto/request/LoginRequest.kt
@@ -1,19 +1,11 @@
-package back.ecommerce.auth.dto.request;
+package back.ecommerce.auth.dto.request
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-public class LoginRequest {
-	@NotBlank(message = "이메일은 필수적으로 필요합니다.")
-	@Email(message = "옳바른 이메일 형식이 아닙니다.")
-	private String email;
-	@NotBlank(message = "비밀번호는 필수적으로 필요합니다.")
-	private String password;
-}
+data class LoginRequest(
+    @field:NotBlank(message = "이메일은 필수적으로 필요합니다.") @Email(message = "옳바른 이메일 형식이 아닙니다.")
+    val email: String,
+    @field:NotBlank(message = "비밀번호는 필수적으로 필요합니다.")
+    val password: String
+)
