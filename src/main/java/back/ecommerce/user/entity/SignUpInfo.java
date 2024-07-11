f18797d58f0dbacfec71000ffbfe5b564575a761
kimJaeHyeon <kmr2644@gmail.com>
2024-07-12 01:23:07 +0900

    refactor: 롬복제거

--- src/main/java/back/ecommerce/user/entity/SignUpInfo.java
+++ src/main/java/back/ecommerce/user/entity/SignUpInfo.java
@@ -7,17 +7,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "sign_up_info")
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
 public class SignUpInfo {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,6 +24,30 @@ public class SignUpInfo {
 	private String verifiedCode;
 	private Long expiredTime;
 	private LocalDateTime createdDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getVerifiedCode() {
+		return verifiedCode;
+	}
+
+	public Long getExpiredTime() {
+		return expiredTime;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
 
 	public static SignUpInfo create(String verifiedCode, String email, String password, Long expiredTime) {
 		return new SignUpInfo(null, email, password, verifiedCode, expiredTime, LocalDateTime.now());
