6dea8d4299d50181f9d05010181b2b098fad50c0
kimJaeHyeon <kmr2644@gmail.com>
2024-07-12 01:24:15 +0900

    refactor : deprecated 설정 삭제

--- src/main/java/back/ecommerce/config/redis/RedisConfig.java
+++ src/main/java/back/ecommerce/config/redis/RedisConfig.java
@@ -1,6 +1,5 @@
 package back.ecommerce.config.redis;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -13,14 +12,9 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableRedisRepositories
 public class RedisConfig {
 
-	@Value("${spring.redis.host}")
-	private String host;
-	@Value("${spring.redis.port}")
-	private int port;
-
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(host, port);
+		return new LettuceConnectionFactory();
 	}
 
 	@Bean
