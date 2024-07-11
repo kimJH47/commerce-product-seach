23e1dfc646b80dfadd4884dfa2b549b54e70057e
kimJaeHyeon <kmr2644@gmail.com>
2024-07-11 16:57:54 +0900

    refactor: dto dta class 로 변경

--- src/main/java/back/ecommerce/cart/dto/response/AddCartResponse.kt
+++ src/main/java/back/ecommerce/cart/dto/response/AddCartResponse.kt
@@ -1,16 +1,7 @@
-package back.ecommerce.cart.dto.response;
+package back.ecommerce.cart.dto.response
 
-import back.ecommerce.cart.entity.Cart;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-public class AddCartResponse {
-	private final Long id;
-	private final int quantity;
-	private final Long price;
-	public static AddCartResponse create(Cart cart) {
-		return new AddCartResponse(cart.getId(),cart.getQuantity(), cart.getPrice());
-	}
-}
+data class AddCartResponse(
+    val id: Long,
+    val quantity: Int,
+    val price: Long
+)
