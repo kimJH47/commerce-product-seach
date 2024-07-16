17fb71f87f03e21e9ff077abf9508c4e97f0cb34
kimJaeHyeon <kmr2644@gmail.com>
2024-07-16 21:05:42 +0900

    refactor: JwtAuthenticationInterceptor 코틀린으로 변경

--- src/main/java/back/ecommerce/api/auth/interceptor/JwtAuthenticationInterceptor.kt
+++ src/main/java/back/ecommerce/api/auth/interceptor/JwtAuthenticationInterceptor.kt
@@ -1,44 +1,38 @@
-package back.ecommerce.api.auth.interceptor;
+package back.ecommerce.api.auth.interceptor
 
-import static back.ecommerce.exception.ErrorCode.*;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
-import org.springframework.web.servlet.HandlerInterceptor;
-
-import back.ecommerce.auth.service.TokenProvider;
-import back.ecommerce.exception.AuthenticationException;
-import lombok.RequiredArgsConstructor;
+import back.ecommerce.auth.service.TokenExtractor
+import back.ecommerce.exception.AuthenticationException
+import back.ecommerce.exception.ErrorCode
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import lombok.RequiredArgsConstructor
+import org.springframework.http.HttpHeaders
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
 @RequiredArgsConstructor
-public class JwtAuthenticationInterceptor implements HandlerInterceptor {
+class JwtAuthenticationInterceptor(
+    private val tokenExtractor: TokenExtractor
+) : HandlerInterceptor {
 
-	private static final String AUTHORIZATION_TYPE = "Bearer ";
-	private static final String EMAIL_ATTRIBUTE = "email";
+    @Throws(Exception::class)
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
 
-	private final TokenProvider tokenProvider;
+        request.getHeader(HttpHeaders.AUTHORIZATION)?.let {
+            if (it.startsWith(AUTHORIZATION_TYPE)) {
+                it.removePrefix(AUTHORIZATION_TYPE)
+            } else throw AuthenticationException(ErrorCode.AUTH_HEADER_INVALID)
+            tokenExtractor.extractClaim(it, EMAIL_ATTRIBUTE)
+        }.also {
+            request.setAttribute(EMAIL_ATTRIBUTE, it)
+        } ?: throw AuthenticationException(ErrorCode.AUTH_HEADER_IS_EMPTY)
 
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws
-		Exception {
-		String token = extractHeaderToToken(request.getHeader(HttpHeaders.AUTHORIZATION));
-		String email = tokenProvider.extractClaim(token, EMAIL_ATTRIBUTE);
-		request.setAttribute(EMAIL_ATTRIBUTE, email);
-		return true;
-	}
+        return true
+    }
 
-	private String extractHeaderToToken(String header) {
-		if (!StringUtils.hasText(header)) {
-			throw new AuthenticationException(AUTH_HEADER_IS_EMPTY);
-		}
-		if (!header.startsWith(AUTHORIZATION_TYPE)) {
-			throw new AuthenticationException(AUTH_HEADER_INVALID);
-		}
-		return header.substring(AUTHORIZATION_TYPE.length());
-	}
+    companion object {
+        private const val AUTHORIZATION_TYPE = "Bearer "
+        private const val EMAIL_ATTRIBUTE = "email"
+    }
 }
