77a7dae20983ce6a5b93fb9b0a559716f42a22c5
kimJaeHyeon <kmr2644@gmail.com>
2024-07-16 21:05:21 +0900

    refactor: 토큰페이로드 분리 메서드 클래스로 분리

--- src/main/java/back/ecommerce/auth/service/TokenProvider.kt
+++ src/main/java/back/ecommerce/auth/service/TokenProvider.kt
@@ -1,9 +1,8 @@
 package back.ecommerce.auth.service
 
 import back.ecommerce.auth.dto.response.Token
-import back.ecommerce.exception.AuthenticationException
-import back.ecommerce.exception.ErrorCode
-import io.jsonwebtoken.*
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.util.*
@@ -13,9 +12,6 @@ class TokenProvider(
     @Value("\${jwt.secretKey}")
     private val securityKey: String
 ) {
-
-    private val parser: JwtParser = Jwts.parser()
-
     fun provide(email: String, expiredTime: Int): Token {
         val payload = HashMap<String, Any>()
         payload["email"] = email
@@ -37,29 +33,9 @@ class TokenProvider(
         return expireDate
     }
 
-    fun extractClaim(token: String?, claimName: String): String {
-        return kotlin.runCatching {
-            parser.setSigningKey(securityKey)
-                .parseClaimsJws(token)
-                .body
-                .get(claimName, String::class.java)
-        }.getAndNullDefaultOrElse("") {
-            when (it) {
-                is SignatureException -> throw AuthenticationException(ErrorCode.TOKEN_HAS_INVALID)
-                is UnsupportedJwtException -> throw AuthenticationException(ErrorCode.TOKEN_HAS_INVALID)
-                is MalformedJwtException -> throw AuthenticationException(ErrorCode.TOKEN_HAS_INVALID)
-                is ExpiredJwtException -> throw AuthenticationException(ErrorCode.TOKEN_HAS_EXPIRED)
-                else -> throw AuthenticationException(ErrorCode.TOKEN_IS_EMPTY)
-            }
-        }
-    }
-
     companion object {
         private const val TYPE = "Bearer"
         private val SIGNATURE_ALGORITHM = SignatureAlgorithm.HS256
     }
 }
 
-inline fun <R, T : R> Result<T>.getAndNullDefaultOrElse(default: T, onFailure: (exception: Throwable) -> R): R {
-    return this.getOrElse { throwable -> onFailure(throwable) } ?: default
-}
