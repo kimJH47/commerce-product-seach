f01d8c2e1d6f94f879062b475185c6ec3d63cffc
kimJaeHyeon <kmr2644@gmail.com>
2024-07-18 18:08:41 +0900

    test: 테스트 분리

--- src/test/java/back/ecommerce/auth/TokenProviderTest.java
+++ src/test/java/back/ecommerce/auth/TokenProviderTest.java
@@ -10,8 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import back.ecommerce.auth.dto.response.Token;
 import back.ecommerce.auth.service.TokenProvider;
-import back.ecommerce.exception.AuthenticationException;
-import back.ecommerce.exception.CustomException;
 import io.jsonwebtoken.Jwts;
 
 @SpringBootTest(classes = TokenProvider.class)
@@ -40,72 +38,72 @@ class TokenProviderTest {
 		assertThat(actual).isEqualTo(email);
 	}
 
-	@Test
-	@DisplayName("토큰에 유효기간이 만료되면 TokenHasExpiredException 이 발생한다.")
-	void validate_expireDate() {
-		//given
-		String email = "dmkl1s@gmail.com";
-		Token token = tokenProvider.provide(email, 0);
-
-		//expect
-		assertThatThrownBy(() -> tokenProvider.extractClaim(token.getValue(), "email"))
-			.isInstanceOf(AuthenticationException.class)
-			.hasMessage("토큰이 만료 되었습니다.");
-	}
-
-	@Test
-	@DisplayName("토큰이 비어있으면 TokenIsEmpty 가 발생 한다")
-	void validate_empty_token() {
-		//expect
-		assertThatThrownBy(() ->
-			tokenProvider.extractClaim(null, "email"))
-			.isInstanceOf(AuthenticationException.class)
-			.hasMessage("토큰이 비어있습니다.");
-	}
-
-	@Test
-	@DisplayName("토큰이 유효하지 않으면 TokenHasInvalidException 이 발생한다.")
-	void validate_invalidToken() {
-		//given
-		String invalidToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
-			+ ".eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ"
-			+ ".SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
-
-		//expect
-		assertThatThrownBy(() -> tokenProvider.extractClaim(invalidToken, "email"))
-			.isInstanceOf(CustomException.class)
-			.hasMessage("토큰이 유효하지 않습니다.");
-	}
-
-	@Test
-	@DisplayName("토큰 페이로드의 클레임이 정상적으로 파싱되어야한다.")
-	void parse_payload() {
-		//given
-		String expected1 = "dmkl1s@gmail.com";
-		String expected2 = "ontfasdmkl1s@gmail.co.kr";
-		int expireTime = 1000 * 60 * 60;
-		Token token1 = tokenProvider.provide(expected1, 10000000);
-		Token token2 = tokenProvider.provide(expected2, 10000000);
-
-		//when
-		String actual1 = tokenProvider.extractClaim(token1.getValue(), "email");
-		String actual2 = tokenProvider.extractClaim(token2.getValue(), "email");
-
-		//then
-		assertThat(actual1).isEqualTo(expected1);
-		assertThat(actual2).isEqualTo(expected2);
-	}
-
-	@Test
-	@DisplayName("토큰의 페이로드에 요청한 값이 존재하지 않으면 빈 문자열이 발생한다.")
-	void parse_payload_tokenHasInvalidException() {
-		//given
-		String expected = "dmkl1s@gmail.com";
-		int expireTime = 1000 * 60 * 60;
-		Token token = tokenProvider.provide(expected, 10000000);
-
-		//expect
-		String actual = tokenProvider.extractClaim(token.getValue(), "email@@");
-		assertThat(actual).isEmpty();
-	}
+	// @Test
+	// @DisplayName("토큰에 유효기간이 만료되면 TokenHasExpiredException 이 발생한다.")
+	// void validate_expireDate() {
+	// 	//given
+	// 	String email = "dmkl1s@gmail.com";
+	// 	Token token = tokenProvider.provide(email, 0);
+	//
+	// 	//expect
+	// 	assertThatThrownBy(() -> tokenProvider.extractClaim(token.getValue(), "email"))
+	// 		.isInstanceOf(AuthenticationException.class)
+	// 		.hasMessage("토큰이 만료 되었습니다.");
+	// }
+	//
+	// @Test
+	// @DisplayName("토큰이 비어있으면 TokenIsEmpty 가 발생 한다")
+	// void validate_empty_token() {
+	// 	//expect
+	// 	assertThatThrownBy(() ->
+	// 		tokenProvider.extractClaim(null, "email"))
+	// 		.isInstanceOf(AuthenticationException.class)
+	// 		.hasMessage("토큰이 비어있습니다.");
+	// }
+	//
+	// @Test
+	// @DisplayName("토큰이 유효하지 않으면 TokenHasInvalidException 이 발생한다.")
+	// void validate_invalidToken() {
+	// 	//given
+	// 	String invalidToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+	// 		+ ".eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ"
+	// 		+ ".SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+	//
+	// 	//expect
+	// 	assertThatThrownBy(() -> tokenProvider.extractClaim(invalidToken, "email"))
+	// 		.isInstanceOf(CustomException.class)
+	// 		.hasMessage("토큰이 유효하지 않습니다.");
+	// }
+	//
+	// @Test
+	// @DisplayName("토큰 페이로드의 클레임이 정상적으로 파싱되어야한다.")
+	// void parse_payload() {
+	// 	//given
+	// 	String expected1 = "dmkl1s@gmail.com";
+	// 	String expected2 = "ontfasdmkl1s@gmail.co.kr";
+	// 	int expireTime = 1000 * 60 * 60;
+	// 	Token token1 = tokenProvider.provide(expected1, 10000000);
+	// 	Token token2 = tokenProvider.provide(expected2, 10000000);
+	//
+	// 	//when
+	// 	String actual1 = tokenProvider.extractClaim(token1.getValue(), "email");
+	// 	String actual2 = tokenProvider.extractClaim(token2.getValue(), "email");
+	//
+	// 	//then
+	// 	assertThat(actual1).isEqualTo(expected1);
+	// 	assertThat(actual2).isEqualTo(expected2);
+	// }
+	//
+	// @Test
+	// @DisplayName("토큰의 페이로드에 요청한 값이 존재하지 않으면 빈 문자열이 발생한다.")
+	// void parse_payload_tokenHasInvalidException() {
+	// 	//given
+	// 	String expected = "dmkl1s@gmail.com";
+	// 	int expireTime = 1000 * 60 * 60;
+	// 	Token token = tokenProvider.provide(expected, 10000000);
+	//
+	// 	//expect
+	// 	String actual = tokenProvider.extractClaim(token.getValue(), "email@@");
+	// 	assertThat(actual).isEmpty();
+	// }
 }
