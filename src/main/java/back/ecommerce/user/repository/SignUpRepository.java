9900a70e685a61c055d6db0b429684ee2f154588
kimJaeHyeon <kmr2644@gmail.com>
2024-07-20 00:15:24 +0900

    refactor: redis 사용 제거

--- src/main/java/back/ecommerce/user/repository/SignUpRepository.java
+++ src/main/java/back/ecommerce/user/repository/SignUpRepository.java
@@ -2,11 +2,12 @@ package back.ecommerce.user.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import back.ecommerce.user.entity.SignUpInfo;
 
-public interface SignUpRepository {
-
-	SignUpInfo save(SignUpInfo signUpInfo);
-
+public interface SignUpRepository extends JpaRepository<SignUpInfo, Long> {
 	Optional<SignUpInfo> findByVerifiedCode(String verifiedCode);
+
+	boolean existsByEmail(String email);
 }
