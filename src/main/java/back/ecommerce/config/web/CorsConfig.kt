dd75106f559597e52f1f5bd7000120e30ac93909
kimJaeHyeon <kmr2644@gmail.com>
2024-07-22 15:40:04 +0900

    feat: CORS config 작성

---
+++ src/main/java/back/ecommerce/config/web/CorsConfig.kt
@@ -0,0 +1,14 @@
+package back.ecommerce.config.web
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedMethods("*")
+            .allowedOrigins("http://localhost:5173");
+    }
+}
