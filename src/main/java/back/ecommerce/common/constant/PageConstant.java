9a133856f152e93ece7d56640f502a108c7e5a5d
kimJaeHyeon <kmr2644@gmail.com>
2024-07-24 23:42:57 +0900

    refactor: DEFAULT_PAGE_SIZE 변경

--- src/main/java/back/ecommerce/common/constant/PageConstant.java
+++ src/main/java/back/ecommerce/common/constant/PageConstant.java
@@ -1,6 +1,6 @@
 package back.ecommerce.common.constant;
 
 public class PageConstant {
-	public static final int DEFAULT_PAGE_SIZE = 20;
+	public static final int DEFAULT_PAGE_SIZE = 12;
 	public static final int DEFAULT__PAGE = 0;
 }
