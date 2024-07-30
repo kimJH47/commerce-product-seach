7b973c89118f66577fd683f9dab81986aff4bf3f
kimJaeHyeon <kmr2644@gmail.com>
2024-07-31 00:59:21 +0900

    feat: 상품 이미지 업로드 api 구현

---
+++ src/main/java/back/ecommerce/api/image/ProductImageController.kt
@@ -0,0 +1,24 @@
+package back.ecommerce.api.image
+
+import back.ecommerce.api.dto.Response
+import back.ecommerce.image.application.ProductImageFacadeService
+import back.ecommerce.image.dto.request.ImageUploadRequest
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/images")
+class ProductImageController(
+    private val productImageFacadeService: ProductImageFacadeService
+) {
+
+    @PostMapping("/products/upload")
+    fun upload(@RequestBody request: ImageUploadRequest): ResponseEntity<Response> {
+        return Response.createSuccessResponse(
+            "이미지가 성공적으로 업로드 되었습니다.", productImageFacadeService.uploads(request)
+        )
+    }
+}
