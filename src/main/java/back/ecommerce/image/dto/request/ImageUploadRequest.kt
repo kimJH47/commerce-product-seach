edcacb44bcf0545bdb800df8340a6ca4fd352101
kimJaeHyeon <kmr2644@gmail.com>
2024-07-31 14:43:44 +0900

    refactor: 이미지 갯수 검증 어노테이션 추가

--- src/main/java/back/ecommerce/image/dto/request/ImageUploadRequest.kt
+++ src/main/java/back/ecommerce/image/dto/request/ImageUploadRequest.kt
@@ -1,10 +1,12 @@
 package back.ecommerce.image.dto.request
 
+import jakarta.validation.constraints.Size
 import org.springframework.web.multipart.MultipartFile
 
 data class ImageUploadRequest(
     val thumbnail: MultipartFile? = null,
     val catalog: MultipartFile? = null,
+    @field:Size(max = 5, message = "상품 이미지의 최대 갯수는 5개 입니다.")
     val images: List<MultipartFile> = emptyList()
 ) {
     fun toMap(): Map<String, Any> = buildMap {
