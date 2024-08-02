7c8dbe8662ff93fad335bfe9ea35f6808d4cc21b
kimJaeHyeon <kmr2644@gmail.com>
2024-08-02 14:04:59 +0900

    refactor: 인터페이스 이름변경, 스펙 필드이름변경, 업로드 예외 IllegalArgumentException 로 변경

--- src/main/java/back/ecommerce/image/infra/S3ImageStorageService.kt
+++ src/main/java/back/ecommerce/image/infra/S3ImageStorageService.kt
@@ -1,6 +1,6 @@
 package back.ecommerce.image.infra
 
-import back.ecommerce.image.application.ImageStorageProcessor
+import back.ecommerce.image.application.ImageStorageService
 import back.ecommerce.image.application.ImageUploadSpec
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.ObjectMetadata
@@ -11,21 +11,21 @@ import org.springframework.stereotype.Service
 class S3ImageStorageService(
     private val amazonS3Client: AmazonS3Client,
     @Value("\${cloud.aws.s3.bucket}") val bucket: String,
-) : ImageStorageProcessor {
+) : ImageStorageService {
 
     override fun upload(spec: ImageUploadSpec): String {
         if (spec !is S3UploadImageSpec) {
-            throw Exception("${spec::class.simpleName} is not valid. Only S3UploadImageSpec instances are allowed.")
+            throw IllegalArgumentException("${spec::class.simpleName} is not valid. Only S3UploadImageSpec instances are allowed.")
         }
         val metadata = ObjectMetadata().apply {
-            contentType = spec.file.contentType
-            contentLength = spec.file.size
+            contentType = spec.image.contentType
+            contentLength = spec.image.size
         }
         try {
-            amazonS3Client.putObject(bucket, spec.filePath, spec.file.inputStream, metadata)
+            amazonS3Client.putObject(bucket, spec.imagePath, spec.image.inputStream, metadata)
         } catch (e: Exception) {
             throw Exception("Error uploading image", e)
         }
-        return amazonS3Client.getUrl(bucket, spec.filePath).toString()
+        return amazonS3Client.getUrl(bucket, spec.imagePath).toString()
     }
 }
