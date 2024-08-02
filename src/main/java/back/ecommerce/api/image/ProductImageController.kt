922c9328f5f75eb1679eaab51934df5dc25b14c1
kimJaeHyeon <kmr2644@gmail.com>
2024-08-02 14:45:31 +0900

    fix: 파라미터 변경

--- src/main/java/back/ecommerce/api/image/ProductImageController.kt
+++ src/main/java/back/ecommerce/api/image/ProductImageController.kt
@@ -1,11 +1,12 @@
 package back.ecommerce.api.image
 
+import back.ecommerce.api.auth.resolver.annotation.UserEmail
 import back.ecommerce.api.dto.Response
 import back.ecommerce.image.application.ProductImageFacadeService
 import back.ecommerce.image.dto.request.ImageUploadRequest
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -16,9 +17,11 @@ class ProductImageController(
 ) {
 
     @PostMapping("/products/upload")
-    fun upload(@RequestBody request: ImageUploadRequest): ResponseEntity<Response> {
+    fun upload(@UserEmail email: String, @ModelAttribute request: ImageUploadRequest): ResponseEntity<Response> {
         return Response.createSuccessResponse(
-            "이미지가 성공적으로 업로드 되었습니다.", productImageFacadeService.uploads(request)
+            "이미지가 성공적으로 업로드 되었습니다.", productImageFacadeService.uploads(
+                email, request
+            )
         )
     }
 }
