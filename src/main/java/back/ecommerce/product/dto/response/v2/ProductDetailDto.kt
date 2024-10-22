979513d7666f7ec432ad6e1f5c2ff3188a9fa5b4
kimJaeHyeon <kmr2644@gmail.com>
2024-10-22 21:22:43 +0900

    refactor: 카테고리 제거

--- src/main/java/back/ecommerce/product/dto/response/v2/ProductDetailDto.kt
+++ src/main/java/back/ecommerce/product/dto/response/v2/ProductDetailDto.kt
@@ -1,13 +1,10 @@
 package back.ecommerce.product.dto.response.v2
 
-import back.ecommerce.product.entity.Category
-
 data class ProductDetailDto(
     val id: Long,
     val name: String,
     val brandName: String,
     val price: Long,
-    val category: Category,
     val imageUrls: List<String>,
     val catalogUrl: String,
 )
