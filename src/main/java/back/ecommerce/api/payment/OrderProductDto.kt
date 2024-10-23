6f59ebe38582c18d0679aa0c36aa4e81c75b954c
kimJaeHyeon <kmr2644@gmail.com>
2024-10-23 16:21:16 +0900

    refactor: data class 로 변경

--- src/main/java/back/ecommerce/api/payment/OrderProductDto.kt
+++ src/main/java/back/ecommerce/api/payment/OrderProductDto.kt
@@ -1,22 +1,16 @@
-package back.ecommerce.api.payment;
+package back.ecommerce.api.payment
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotNull
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+data class OrderProductDto(
+    @field:NotNull(message = "상품의 아이디는 필수적으로 필요합니다.")
+    val productId: Long,
+    @field:NotNull(message = "주문상품의 이름은 필수적으로 필요합니다.")
+    val name: String,
+    @field:Min(value = 1, message = "상품의 갯수는 최소 1개 이상이여야 합니다.")
+    val quantity: Int,
+    @field:NotNull(message = "가격은 필수적으로 필요 합니다.")
+    val price: Long
+)
 
-@AllArgsConstructor
-@Getter
-@NoArgsConstructor
-public class OrderProductDto {
-	@NotNull(message = "상품의 아이디는 필수적으로 필요합니다.")
-	private Long productId;
-	private String name;
-	@Min(value = 1,message = "상품의 갯수는 최소 1개 이상이여야 합니다.")
-	private Integer quantity;
-	@Min(value = 1,message = "상품의 갯수는 최소 1개 이상이여야 합니다.")
-	@NotNull(message = "가격은 필수적으로 필요 합니다.")
-	private Long price;
-}
