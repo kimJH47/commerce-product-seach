35cd34594baa251713316a33b96600f896f2fdd6
kimJaeHyeon <kmr2644@gmail.com>
2024-10-23 15:16:19 +0900

    refactor: 의존 서비스 V2 로 변경

--- src/main/java/back/ecommerce/api/product/v2/ProductV2Controller.kt
+++ src/main/java/back/ecommerce/api/product/v2/ProductV2Controller.kt
@@ -3,9 +3,9 @@ package back.ecommerce.api.product.v2
 import back.ecommerce.api.dto.Response
 import back.ecommerce.api.resolver.ProductSearchRequestMapping
 import back.ecommerce.common.logging.aop.annotation.Logging
+import back.ecommerce.product.application.ProductV2Service
 import back.ecommerce.product.dto.request.ProductSearchConditionRequest
 import back.ecommerce.product.entity.Category
-import back.ecommerce.product.service.ProductService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -15,14 +15,14 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v2")
 class ProductV2Controller(
-    val productService: ProductService
+    val productV2Service: ProductV2Service
 ) {
 
     @GetMapping("/categories/{category}")
     fun findProductsByCategory(@PathVariable("category") category: String): ResponseEntity<Response> {
         return Response.createSuccessResponse(
             "상품이 성공적으로 조회 되었습니다.",
-            productService.findWithCategoryAndPagination(Category.from(category))
+            productV2Service.findWithCategoryAndPagination(Category.from(category))
         )
     }
 
@@ -32,12 +32,12 @@ class ProductV2Controller(
             : ResponseEntity<Response> {
         return Response.createSuccessResponse(
             "상품이 성공적으로 조회 되었습니다.",
-            productService.findWithSearchCondition(request.category, request.parameters)
+            productV2Service.findWithSearchCondition(request.category, request.parameters)
         )
     }
 
     @GetMapping("/product/{id}")
     fun findOne(@PathVariable("id") id: Long): ResponseEntity<Response> {
-        return Response.createSuccessResponse("상품이 성공적으로 조회 되었습니다.", productService.findOne(id))
+        return Response.createSuccessResponse("상품이 성공적으로 조회 되었습니다.", productV2Service.findOne(id))
     }
 }
