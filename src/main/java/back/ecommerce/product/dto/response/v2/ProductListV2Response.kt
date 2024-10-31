bb81512c4e1bf9f715eaa67117653f56ea123884
kimJaeHyeon <kmr2644@gmail.com>
2024-10-31 15:31:41 +0900

    fix: 잘못된 import 삭제

--- src/main/java/back/ecommerce/product/dto/response/v2/ProductListV2Response.kt
+++ src/main/java/back/ecommerce/product/dto/response/v2/ProductListV2Response.kt
@@ -1,9 +1,8 @@
 package back.ecommerce.product.dto.response.v2
 
-import okhttp3.internal.immutableListOf
 
 //thumbnail url 응답추가
 data class ProductListV2Response(
     val totalCount: Int,
-    val products: List<ProductV2Dto> = immutableListOf(),
+    val products: List<ProductV2Dto>
 )
