e7892cb8c968739ba3f9d113620d1b2153222d4a
kimJaeHyeon <kmr2644@gmail.com>
2024-10-31 14:58:31 +0900

    refactor: 패키지 이동

--- src/main/java/back/ecommerce/common/logging/config/LoggerConfig.java
+++ src/main/java/back/ecommerce/common/logging/config/LoggerConfig.java
@@ -1,4 +1,4 @@
-package back.ecommerce.common.config.logging;
+package back.ecommerce.common.logging.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
