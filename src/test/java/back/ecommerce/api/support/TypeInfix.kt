d2bb8f7e777e8a3e7e67276e2c61a940705642b2
kimJaeHyeon <kmr2644@gmail.com>
2024-11-12 14:21:00 +0900

    feat: QueryParam 생성 함수 구현

--- src/test/java/back/ecommerce/api/support/TypeInfix.kt
+++ src/test/java/back/ecommerce/api/support/TypeInfix.kt
@@ -2,6 +2,8 @@ package back.ecommerce.api.support
 
 import org.springframework.restdocs.payload.JsonFieldType
 import org.springframework.restdocs.payload.PayloadDocumentation
+import org.springframework.restdocs.request.RequestDocumentation
+import org.springframework.restdocs.snippet.Attributes
 
 
 infix fun String.type(docsFieldType: DocsFieldType): Field {
@@ -22,4 +24,18 @@ private fun createField(value: String, type: JsonFieldType, optional: Boolean = 
     if (optional) descriptor.optional()
 
     return Field(descriptor)
+}
+
+infix fun String.query(docsFieldType: DocsFieldType): QueryParam {
+    val createQueryParam = createQueryParam(this, docsFieldType.type)
+    return createQueryParam
+}
+
+private fun createQueryParam(value: String, type: JsonFieldType, optional: Boolean = false): QueryParam {
+    val descriptor = RequestDocumentation.parameterWithName(value)
+        .attributes(Attributes.key("type").value(type.name))
+
+    if (optional) descriptor.optional()
+
+    return QueryParam(descriptor)
 }
