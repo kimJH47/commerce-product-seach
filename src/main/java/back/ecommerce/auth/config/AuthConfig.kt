9f06080895a5f0603bc9f4d78c113d0a1a3e7bd1
kimJaeHyeon <kmr2644@gmail.com>
2024-11-14 13:50:29 +0900

    feat: AuthConfig 임시 커밋

---
+++ src/main/java/back/ecommerce/auth/config/AuthConfig.kt
@@ -0,0 +1,42 @@
+package back.ecommerce.auth.config
+
+import back.ecommerce.auth.filter.JwtAuthenticationFilter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+@EnableWebSecurity
+class AuthConfig(
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter
+) {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.formLogin {
+            it.disable()
+        }.httpBasic {
+            it.disable()
+        }.csrf {
+            it.disable()
+        }.logout {
+            it.disable()
+        }.authorizeHttpRequests {
+            it.requestMatchers(
+                "/api/login",
+                "/api/signup",
+                "/api/auth/exist-nickname",
+                "/api/chat-rooms/exit",
+                "/api/chat-rooms/participant"
+            ).permitAll()
+            //it.requestMatchers("/api/**").authenticated()
+        }.sessionManagement {
+            it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        }.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+
+        return http.build()
+    }
+}
