9f5d03d17a114e4e2fda66b6489ddc0566b443ae
kimJaeHyeon <kmr2644@gmail.com>
2024-11-14 15:56:57 +0900

    refactor: Component 어노테이션 제거, 빈 config 등록

--- src/main/java/back/ecommerce/auth/filter/JwtAuthenticationFilter.kt
+++ src/main/java/back/ecommerce/auth/filter/JwtAuthenticationFilter.kt
@@ -7,10 +7,8 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpHeaders
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 
-@Component
 class JwtAuthenticationFilter(
     private val tokenExtractor: TokenExtractor,
     private val authenticationProvider: JwtAuthenticationProvider,
