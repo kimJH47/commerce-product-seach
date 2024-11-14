4fc5c651653919a9694c73ef09c75ebc6609eef4
kimJaeHyeon <kmr2644@gmail.com>
2024-11-14 16:05:10 +0900

    refactor: AuthenticationPrincipal 사용하게 변경

--- src/main/java/back/ecommerce/api/image/ProductImageController.kt
+++ src/main/java/back/ecommerce/api/image/ProductImageController.kt
@@ -1,10 +1,11 @@
 package back.ecommerce.api.image
 
-import back.ecommerce.api.auth.resolver.annotation.UserEmail
 import back.ecommerce.api.dto.Response
+import back.ecommerce.auth.domain.AuthUser
 import back.ecommerce.image.application.ProductImageFacadeService
 import back.ecommerce.image.dto.request.ImageUploadRequest
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,10 +18,10 @@ class ProductImageController(
 ) {
 
     @PostMapping("/products/upload")
-    fun upload(@UserEmail email: String, @ModelAttribute request: ImageUploadRequest): ResponseEntity<Response> {
+    fun upload(@AuthenticationPrincipal authUser: AuthUser, @ModelAttribute request: ImageUploadRequest): ResponseEntity<Response> {
         return Response.createSuccessResponse(
             "이미지가 성공적으로 업로드 되었습니다.", productImageFacadeService.uploads(
-                email, request
+                authUser.username, request
             )
         )
     }
