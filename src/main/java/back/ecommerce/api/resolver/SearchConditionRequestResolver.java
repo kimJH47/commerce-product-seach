6f071397d4eeede3addcdc2f3982654ca845ad59
kimJaeHyeon <kmr2644@gmail.com>
2024-11-15 01:07:30 +0900

    refactor: 카테고리 파싱 로직 변경

--- src/main/java/back/ecommerce/api/resolver/SearchConditionRequestResolver.java
+++ src/main/java/back/ecommerce/api/resolver/SearchConditionRequestResolver.java
@@ -13,13 +13,11 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 import back.ecommerce.product.dto.request.ProductSearchConditionRequest;
 import back.ecommerce.product.entity.Category;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public class SearchConditionRequestResolver implements HandlerMethodArgumentResolver {
 
-	private static final int CATEGORY_INDEX = 4;
 	private static final String URI_SPLIT_REGEX = "/";
+	private static final String URL_CRITERIA = "categories";
 
 	@Override
 	public boolean supportsParameter(MethodParameter parameter) {
@@ -30,13 +28,14 @@ public class SearchConditionRequestResolver implements HandlerMethodArgumentReso
 	public ProductSearchConditionRequest resolveArgument(@NotNull MethodParameter parameter,
 		ModelAndViewContainer mavContainer,
 		@NotNull NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-		return ProductSearchConditionRequest.Companion.create(extractCategory(webRequest), extractParameterToMap(webRequest));
+		return ProductSearchConditionRequest.Companion.create(extractCategory(webRequest),
+			extractParameterToMap(webRequest));
 	}
 
 	private Category extractCategory(NativeWebRequest webRequest) {
 		HttpServletRequest request = (HttpServletRequest)webRequest.getNativeRequest();
 		String requestURI = request.getRequestURI();
-		return Category.from(requestURI.split(URI_SPLIT_REGEX)[CATEGORY_INDEX]);
+		return extractCategory(requestURI);
 	}
 
 	private Map<String, String> extractParameterToMap(NativeWebRequest webRequest) {
@@ -48,5 +47,17 @@ public class SearchConditionRequestResolver implements HandlerMethodArgumentReso
 			}
 		});
 		return attributes;
+	}
+
+	private Category extractCategory(String url) {
+		String[] split = url.split(URI_SPLIT_REGEX);
+		int index = 0;
+		for (int i = 0; i < split.length; i++) {
+			if (split[i].equals(URL_CRITERIA)) {
+				index = i + 1;
+				break;
+			}
+		}
+		return Category.from(split[index]);
 	}
 }
