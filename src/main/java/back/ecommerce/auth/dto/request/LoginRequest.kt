e1acabb4565b104ae610955f6e749b87a93c730f
kimJaeHyeon <kmr2644@gmail.com>
2024-11-15 01:25:21 +0900

    fix: field 추가

--- src/main/java/back/ecommerce/auth/dto/request/LoginRequest.kt
+++ src/main/java/back/ecommerce/auth/dto/request/LoginRequest.kt
@@ -4,7 +4,8 @@ import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 
 data class LoginRequest(
-    @field:NotBlank(message = "이메일은 필수적으로 필요합니다.") @Email(message = "옳바른 이메일 형식이 아닙니다.")
+    @field:NotBlank(message = "이메일은 필수적으로 필요합니다.")
+    @field:Email(message = "옳바른 이메일 형식이 아닙니다.")
     val email: String,
     @field:NotBlank(message = "비밀번호는 필수적으로 필요합니다.")
     val password: String
